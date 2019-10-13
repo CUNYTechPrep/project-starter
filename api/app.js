b518c3aa8ad194b74e487834a90fc8980d6554e0
Samuel Witke <samuel.witke@cigna.com>
2019-10-12 21:30:15 -0400

    Added ESlint to backend and CICD pre-push

--- api/app.js
+++ api/app.js
@@ -3,30 +3,30 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const path = require('path');
 const db = require('./models');
+
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-
 // this lets us parse 'application/json' content in http requests
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 // add http request logging to help us debug and audit app use
-const logFormat = process.env.NODE_ENV==='production' ? 'combined' : 'dev';
+const logFormat = process.env.NODE_ENV === 'production' ? 'combined' : 'dev';
 app.use(morgan(logFormat));
 
 // this mounts controllers/index.js at the route `/api`
 app.use('/api', require('./controllers'));
 
 // for production use, we serve the static react build folder
-if(process.env.NODE_ENV==='production') {
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 
-  // all unknown routes should be handed to our react app
-  app.get('*', function (req, res) {
+  // all unknown routes shou:w
+  app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
   });
 }
-
+plz break
 // update DB tables based on model updates. Does not handle renaming tables/columns
 // NOTE: toggling this to true drops all tables (including data)
 db.sequelize.sync({ force: false });
