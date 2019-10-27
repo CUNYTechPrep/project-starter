d2f05e2811d6a80bbc8d7bbe26cb5482e0898267
Shahzoda Davlatova <shahzoda01@gmail.com>
2019-10-27 13:52:47 -0400

    db seeded and now the user endpoints

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -3,13 +3,15 @@ const router = express.Router();
 
 
 // Load each controller
-const postsController = require('./posts.js');
 const appConfigController = require('./appConfig.js');
+const postsController = require('./posts.js');
+const usersController = require('./users.js');
 
 // Mount each controller under a specific route. These
 // will be prefixes to all routes defined inside the controller
-router.use('/posts', postsController);
 router.use('/application-configuration', appConfigController);
+router.use('/posts', postsController);
+router.use('/users', usersController); 
 
 
 module.exports = router;
--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,23 +1,26 @@
-// const express = require('express');
-// const router = express.Router();
-// const db = require('../models');
-// const { User } = db;
+const express = require('express');
+const router = express.Router();
+const db = require('../models');
+const { User } = db;
+const seed = require('../seed');
 
-// // This is a simple example for providing basic CRUD routes for
-// // a resource/model. It provides the following:
-// //    POST   /user -> creates new user (registering)
-// //    GET    /users/:id
-// //    PUT    /user/:id
-// //    DELETE /user/:id 
+// This is a simple example for providing basic CRUD routes for
+// a resource/model. It provides the following:
+//    POST   /user -> creates new user (registering)
+//    GET    /users/:id
+//    PUT    /user/:id
+//    DELETE /user/:id 
 
-// // Note to self:
-// //  {x, y} = foo;
-// //  x = foo.x;
-// //  y = foo.y;
+// Note to self:
+//  {x, y} = foo;
+//  x = foo.x;
+//  y = foo.y;
 
-// router.get('/', (req,res) => {
-//     User.findAll({})
-//         .then(posts => res.json(posts));
-// });
+seed()
 
-// module.exports = router;
+router.get('/', (req,res) => {
+    User.findAll({})
+        .then(users => res.json(users));
+});
+
+module.exports = router;
