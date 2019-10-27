69545f9c58179f8a229a8f11b5763545474ba1c1
Shahzoda Davlatova <shahzoda01@gmail.com>
2019-10-27 13:23:34 -0400

    express endoint /application-configuration and /posts now work

--- api/controllers/appConfig.js
+++ api/controllers/appConfig.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (res, req) => {
+router.get('/', (req, res) => {
   res.json({
     title: 'APP TITLE',
     description: 'A short description about this app',
