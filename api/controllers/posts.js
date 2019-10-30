153ad82fcb1cd9ad4033335a9f4d93d40e4ac7fc
Edgardo Molina <medgardo@gmail.com>
2019-10-30 03:49:33 -0400

    Add api endpoint protection to the posts controller

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models');
+const passport = require('../middlewares/authentication');
 const { Post } = db;
 
 // This is a simple example for providing basic CRUD routes for
@@ -22,17 +23,20 @@ router.get('/', (req,res) => {
 });
 
 
-router.post('/', (req, res) => {
-  let { content } = req.body;
-  
-  Post.create({ content })
-    .then(post => {
-      res.status(201).json(post);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
-});
+router.post('/',
+  passport.isAuthenticated(),
+  (req, res) => {
+    let { content } = req.body;
+    
+    Post.create({ content })
+      .then(post => {
+        res.status(201).json(post);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  }
+);
 
 
 router.get('/:id', (req, res) => {
@@ -48,38 +52,44 @@ router.get('/:id', (req, res) => {
 });
 
 
-router.put('/:id', (req, res) => {
-  const { id } = req.params;
-  Post.findByPk(id)
-    .then(post => {
-      if(!post) {
-        return res.sendStatus(404);
-      }
+router.put('/:id',
+  passport.isAuthenticated(),
+  (req, res) => {
+    const { id } = req.params;
+    Post.findByPk(id)
+      .then(post => {
+        if(!post) {
+          return res.sendStatus(404);
+        }
 
-      post.content = req.body.content;
-      post.save()
-        .then(post => {
-          res.json(post);
-        })
-        .catch(err => {
-          res.status(400).json(err);
-        });
-    });
-});
+        post.content = req.body.content;
+        post.save()
+          .then(post => {
+            res.json(post);
+          })
+          .catch(err => {
+            res.status(400).json(err);
+          });
+      });
+  }
+);
 
 
-router.delete('/:id', (req, res) => {
-  const { id } = req.params;
-  Post.findByPk(id)
-    .then(post => {
-      if(!post) {
-        return res.sendStatus(404);
-      }
+router.delete('/:id',
+  passport.isAuthenticated(),
+  (req, res) => {
+    const { id } = req.params;
+    Post.findByPk(id)
+      .then(post => {
+        if(!post) {
+          return res.sendStatus(404);
+        }
 
-      post.destroy();
-      res.sendStatus(204);
-    });
-});
+        post.destroy();
+        res.sendStatus(204);
+      });
+  }
+);
 
 
 module.exports = router;
