db6ad4e5548c3ff5728db27df1cc030610e27d1c
Edgardo Molina <medgardo@gmail.com>
2019-10-30 01:41:29 -0400

    Add /api/auth routes for signup, login, and logout

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -3,11 +3,13 @@ const router = express.Router();
 
 
 // Load each controller
+const authController = require('./auth');
 const postsController = require('./posts.js');
 const appConfigController = require('./appConfig.js');
 
 // Mount each controller under a specific route. These
 // will be prefixes to all routes defined inside the controller
+router.use('/auth', authController);
 router.use('/posts', postsController);
 router.use('/application-configuration', appConfigController);
 
