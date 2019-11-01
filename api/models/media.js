b934437afb7569282939e587a6f745461dca3692
Shahzoda Davlatova <shahzoda01@gmail.com>
2019-11-01 13:21:40 -0400

    media model added

--- api/models/media.js
+++ api/models/media.js
@@ -1,40 +1,47 @@
-'use strict';
-const { Model } = require('sequelize');
+// 'use strict';
+// const { Model } = require('sequelize');
 
 
-module.exports = (sequelize, DataTypes) => {
-    class Media extends Model {}
-    Media.init({
-        name: {
-            type: DataTypes.STRING,
-            validate: {
-                notEmpty: true
-            }
-        },
-        description: {
-            type: DataTypes.STRING,
-            validate: {
-                len: [3, 500],
-                notEmpty: true,
-            }
-        },
-        coverPhoto: {
-            type: DataTypes.STRING, 
-            validate: {
-                len: [3, 10],
-                notEmpty: true,
-                isEmail: true,
-            }
-        }, 
-    }, {
-        sequelize,
-        modelName: 'Media'
-    });
+// module.exports = (sequelize, DataTypes) => {
+//     class Media extends Model {}
+//     Media.init({
+//         name: {
+//             type: DataTypes.STRING,
+//             validate: {
+//                 notEmpty: true
+//             }
+//         },
+//         description: {
+//             type: DataTypes.STRING,
+//             validate: {
+//                 len: [3, 500],
+//                 notEmpty: true,
+//             }
+//         },
+//         coverPhoto: {
+//             type: DataTypes.STRING, //What data type to carry images?
+//             validate: {
+//                 len: [3, 10],
+//                 notEmpty: true,
+//                 isEmail: true,
+//             }
+//         }, 
+//         bio: {
+//             type: DataTypes.TEXT
+//         },
+//     }, {
+//         sequelize,
+//         modelName: 'Media'
+//     });
 
-     Media.associate = (models) => {
-        // This adds TripID to Media 
-        models.Media.belongsTo(models.Trip);
-     };
+//      Media.associate = (models) => {
+//         models.Media.belongsTo(models.Trip);\
 
-    return Media;
-};
+        
+//         // associations can be defined here
+//         // This will add MediaId as a column to the Media table
+         
+//      };
+
+//     return Media;
+// };
