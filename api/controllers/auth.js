123cdb1bd0f12f28378fa94996486c92b003929e
mankenconde <manken.conde@cix.csi.cuny.edu>
2019-11-03 00:44:48 -0400

    company delete route finished

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -11,38 +11,38 @@ const { Company, User } = db;
 //@desc     Get current user
 //@access   Private
 router.get("/users", auth, async (req, res) => {
-    if (!req.user) {
-        return res.status(401).json({ msg: "Unauthorized" });
-    }
-    try {
-        const { id } = req.user;
-        const user = await User.findByPk(id, {
-            attributes: { exclude: ["password"] }
-        });
-        res.json(user);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ msg: "Server error" });
-    }
+  if (!req.user) {
+    return res.status(401).json({ msg: "Unauthorized" });
+  }
+  try {
+    const { id } = req.user;
+    const user = await User.findByPk(id, {
+      attributes: { exclude: ["password"] }
+    });
+    res.json(user);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ msg: "Server error" });
+  }
 });
 
 //@route    GET api/auth/company
 //@desc     Get current company
 //@access   Private
 router.get("/companies", auth, async (req, res) => {
-    if (!req.company) {
-        return res.status(401).json({ msg: "Unauthorized" });
-    }
-    try {
-        const { id } = req.company;
-        const company = await Company.findByPk(id, {
-            attributes: { exclude: ["password"] }
-        });
-        res.json(company);
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ msg: "Server error" });
-    }
+  if (!req.company) {
+    return res.status(401).json({ msg: "Unauthorized" });
+  }
+  try {
+    const { id } = req.company;
+    const company = await Company.findByPk(id, {
+      attributes: { exclude: ["password"] }
+    });
+    res.json(company);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ msg: "Server error" });
+  }
 });
 
 //@route    POST api/auth/users
@@ -115,76 +115,6 @@ router.post(
       res.status(500).json({ msg: "Server error" });
     }
   }
-
-    "/users",
-    [
-        check("email", "Please enter a valid email").isEmail(),
-        check("password", "Please enter a password").exists()
-    ],
-    async (req, res) => {
-        const errors = validationResult(req);
-
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
-
-        const { email, password } = req.body;
-
-        try {
-            //Find user by email
-            const user = await User.findOne({ where: { email } });
-
-            //If no user was found
-            if (!user) {
-                return res.status(400).json({
-                    errors: [
-                        {
-                            params: "invalidCredentials",
-                            msg: "Invalid Credentials"
-                        }
-                    ]
-                });
-            }
-
-            //Compares password - returns true or false
-            const passwordMatches = await bcrypt.compare(
-                password,
-                user.password
-            );
-
-            //If passwords don't match
-            if (!passwordMatches) {
-                return res.status(400).json({
-                    errors: [
-                        {
-                            params: "invalidCredentials",
-                            msg: "Invalid Credentials"
-                        }
-                    ]
-                });
-            }
-
-            const payload = {
-                user: {
-                    id: user.id
-                }
-            };
-
-            //return payload
-            jwt.sign(
-                payload,
-                process.env.secret,
-                { expiresIn: "7 days" },
-                (err, token) => {
-                    if (err) throw err;
-                    res.json({ token });
-                }
-            );
-        } catch (err) {
-            console.error(err);
-            res.status(500).json({ msg: "Server error" });
-        }
-    }
 );
 
 //@route    POST api/auth/companies
@@ -263,21 +193,43 @@ router.post(
 //@desc     Delete user
 //@access   Private
 router.delete("/users", auth, async (req, res) => {
-    try {
-        if (!req.user) {
-            return res.status(401).json({ msg: "Unauthorized" });
-        }
-        const { id } = req.user;
-        const user = await User.findByPk(id);
-        if (!user) {
-            return res.status(404).json({ msg: "User does not exist" });
-        }
-        await user.destroy();
-        res.json({ msg: "User deleted" });
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ msg: "Server error" });
+  try {
+    if (!req.user) {
+      return res.status(401).json({ msg: "Unauthorized" });
     }
+    const { id } = req.user;
+    const user = await User.findByPk(id);
+    if (!user) {
+      return res.status(404).json({ msg: "User does not exist" });
+    }
+    await user.destroy();
+    res.json({ msg: "User deleted" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ msg: "Server error" });
+  }
+});
+
+//@route    DELETE api/auth/companies
+//@desc     Delete company
+//@access   Private
+router.delete("/companies", auth, async (req, res) => {
+  try {
+    if (!req.company) {
+      return res.status(401).json({ msg: "Unauthorized" });
+    }
+    const { id } = req.company;
+    const company = await Company.findByPk(id);
+    if (!company) {
+      return res.status(404).json({ msg: "Company does not exist" });
+    }
+
+    await company.destroy();
+    res.json({ msg: "Company deleted" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: "Server error" });
+  }
 });
 
 module.exports = router;
