43fff83bc7b49f85f6c3d98634712584e96c05db
Kev <kdowdy1021@gmail.com>
2019-11-05 18:14:13 -0500

    Created the html for the forms

---
+++ client/src/forms/business-form.js
@@ -0,0 +1,35 @@
+<div id="business-form">
+	<form>
+		First Name<br />
+		<input type="text" placeholder="John" required/>
+
+		Last Name<br />
+		<input type="text" placeholder="Doe" required/>
+
+		Email Address<br />
+		<input type="text" placeholder="jdoe@example.com" required/>
+
+		Business Name<br />
+		<input type="text" placeholder="Reentry Org" required/>
+
+		City<br />
+		<input type="text" placeholder="Buffalo" />
+
+		State<br />
+		<input type="text" placeholder="New York" required/>
+
+		Zip Code<br />
+		<input type="text" placeholder="10450" required/>
+
+		Looking For (select as many as apply)<br />
+		<input type="checkbox" name="looking-for" value="GD">Graphic Designer<br>
+		<input type="checkbox" name="looking-for" value="PA">Painter<br>
+		<input type="checkbox" name="looking-for" value="EL">Electrician<br>
+		<input type="checkbox" name="looking-for" value="CA">Carpenter<br>
+		<input type="checkbox" name="looking-for" value="FS">Food Servicer<br>
+		<input type="checkbox" name="looking-for" value="OT">Other<br>
+		
+
+		<input type="submit" value="Submit" />
+	</form>
+</div>
