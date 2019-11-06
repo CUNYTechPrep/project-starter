8870d1da34a8fed3c0bd49f1b1ea8a8ca9506a29
Ellie <j.chen.ellie@gmail.com>
2019-11-06 16:52:48 -0500

    Navigation finished

---
+++ client/src/pages/Landing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Landing extends React.Component {
+    render() {
+        return <div>
+            <span>I'm in Landing!</span> 
+        </div>
+    }
+}
+
+export default Landing;
