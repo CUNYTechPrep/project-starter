0718534683fa59a1724f5e82d8a9b6d0aed1b56e
Rehman Arshad <rehman.arshad777@gmail.com>
2019-11-17 21:51:54 -0500

    npm packages installed, bugs still need fixing

--- client/src/pages/DashboardPage.js
+++ client/src/pages/DashboardPage.js
@@ -1,14 +1,69 @@
 import React, { Component } from 'react';
-import SideBar from '../components/navbar-component';
+import SideBar from '../components/sidebar-component';
 import TopBar from '../components/topbar-component';
-import LogOutModal from '../components/modals-component';
+
+import '../css/dashboard-style.css';
+import '../css/style.css';
+import Tsundere_Meme from '../images/Tsundere-Meme.png';
+
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal'
+import ModalDialog from 'react-bootstrap/ModalDialog'
+import ModalHeader from 'react-bootstrap/ModalHeader'
+import ModalTitle from 'react-bootstrap/ModalTitle'
+import ModalBody from 'react-bootstrap/ModalBody'
+import ModalFooter from 'react-bootstrap/ModalFooter'
+
+
+
+
+
 
 export default class DashboardPage extends Component {
     constructor(props) {
         super(props);
-        this.state = { email: '', password: '' };
+
+        this.LogOutModal = this.LogOutModal.bind(this);
+
+        this.state = { 
+            email: '', 
+            password: '' 
+        };
+         
     }
   
+
+
+    LogOutModal() {
+        const [show, setShow] = React.useState(false);
+      
+        const handleClose = () => React.setShow(false);
+        const handleShow = () => React.setShow(true);
+      
+        return (
+          <div>
+            <Button variant="primary" onClick={handleShow}>
+              Launch demo modal
+            </Button>
+      
+            <Modal show={show} onHide={handleClose}>
+              <Modal.Header closeButton>
+                <Modal.Title>Modal heading</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                  Close
+                </Button>
+                <Button variant="primary" onClick={handleClose}>
+                  Save Changes
+                </Button>
+              </Modal.Footer>
+            </Modal>
+          </div>
+        );
+      }
+
 
 
     render() {
@@ -16,8 +71,9 @@ export default class DashboardPage extends Component {
             <div>
                 <SideBar/>          {/* This is the side bar navbar */}
                 <TopBar/>           {/* This is the top bar navbar */}
-                <LogOutModal/>      {/* This is the sign out modals */}
-                
+                <div className="mt-auto ml-auto ">
+                    <this.LogOutModal />
+                </div>
             </div>
         );
     }
