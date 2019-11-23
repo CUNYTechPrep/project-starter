64c0237eb72c853e8574ba9f0be913ea1bb04dee
Rehman Arshad <rehman.arshad777@gmail.com>
2019-11-22 21:54:41 -0500

    Got LogOut Redirect to work correctly!

--- client/src/pages/DashboardPage.js
+++ client/src/pages/DashboardPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import SideBar from '../components/sidebar-component';
 import TopBar from '../components/topbar-component';
 import Cards from '../components/card-component';
@@ -9,13 +10,25 @@ import TableDark from '../components/table-dark-component';
 export default class DashboardPage extends Component {
     constructor(props) {
         super(props);
-        this.state = { email: '', password: '' };
+        this.state = { 
+            Loggedin: true, 
+            AuthToken: "Authentication Token",
+            username: "",
+        };
     }
-  
+
+    // Get Token from database store it here, if the user is logged in for too long, 
+    // Reset the Authentication Token on the back end 
+    // If the old token doesn't match with the new one (which it shouldn't) 
+    // Then redirect to sign in page
 
 
     render() {
-      return (
+
+        if(this.state.Loggedin === false) return <Redirect to="/login" />
+
+
+        return (
             <div>
                 <SideBar/>          {/* This is the side bar navbar component */}
                 <TopBar/>           {/* This is the top bar navbar component it also contains the modal-component*/}
