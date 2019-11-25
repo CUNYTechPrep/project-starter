d060995b7bd652e4dee625a64ef12bfeef3632c1
Genna Lipkin <genlipkin@gmail.com>
2019-11-25 01:56:56 -0500

    Connect to users controller

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -6,10 +6,16 @@ const router = express.Router();
 const postsController = require('./posts.js');
 const appConfigController = require('./appConfig.js');
 
+const usersController = require('./users.js');
+
+
 // Mount each controller under a specific route. These
 // will be prefixes to all routes defined inside the controller
 router.use('/posts', postsController);
 router.use('/application-configuration', appConfigController);
+
+router.use('/users', usersController);
+
 
 
 module.exports = router;
