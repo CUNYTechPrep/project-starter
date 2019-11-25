ff140275b5b150efd5098e16be0ffa31e9e06308
Genna Lipkin <genlipkin@gmail.com>
2019-11-25 02:01:37 -0500

    Update app.js

--- api/app.js
+++ api/app.js
@@ -1,40 +1,3 @@
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const morgan = require('morgan');
-// const path = require('path');
-// const db = require('./models');
-// const app = express();
-// const PORT = process.env.PORT || 8000;
-
-// //console.log(db);
-// // this lets us parse 'application/json' content in http requests
-// app.use(bodyParser.json())
-
-// // add http request logging to help us debug and audit app use
-// const logFormat = process.env.NODE_ENV==='production' ? 'combined' : 'dev';
-// app.use(morgan(logFormat));
-
-// // this mounts controllers/index.js at the route `/api`
-// app.use('/api', require('./controllers'));
-
-// // for production use, we serve the static react build folder
-// if(process.env.NODE_ENV==='production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
-
-//   // all unknown routes should be handed to our react app
-//   app.get('*', function (req, res) {
-//     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-//   });
-// }
-
-// // update DB tables based on model updates. Does not handle renaming tables/columns
-// // NOTE: toggling this to true drops all tables (including data)
-// db.sequelize.sync({ force: true });
-
-// // start up the server
-// app.listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
@@ -43,7 +6,7 @@ const db = require('./models');
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-
+//console.log(db);
 // this lets us parse 'application/json' content in http requests
 app.use(bodyParser.json())
 
@@ -66,7 +29,7 @@ if(process.env.NODE_ENV==='production') {
 
 // update DB tables based on model updates. Does not handle renaming tables/columns
 // NOTE: toggling this to true drops all tables (including data)
-db.sequelize.sync({ force: false });
+db.sequelize.sync({ force: true });
 
 // start up the server
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
