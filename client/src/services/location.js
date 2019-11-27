d263f31abb9b4bdc59199ea78060f44ac390142b
Julio Juarez <juliojuar1693@gmail.com>
2019-11-27 12:47:37 -0500

    Add client changes for location

--- client/src/services/location.js
+++ client/src/services/location.js
@@ -45,15 +45,5 @@ export default location;
 import locations from '../services/location';
 
 onClickHandler = (event) => {
-  location.getCurrLocation((pos) => {
-    var crd = pos.coords;
-    var lng = pos.coords.longitude;
-    var lat = pos.coords.latitude;
-    fetch('/api/places?lat='+lat+'2&lng='+lng+'&keyword=pizza')
-    .then(res => res.json())
-    .then(res => {
-      console.log(res)
-      this.setState({ ....... })
-    })
-  })
+
 }
