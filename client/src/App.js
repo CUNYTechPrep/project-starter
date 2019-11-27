cb3eb62a434f1336781cd0a98571f86997b3bb2e
Adrian Barros <adriansbarros1@gmail.com>
2019-11-27 13:49:54 -0500

    css

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { 
-  BrowserRouter as Router, 
-  Switch, 
-  Route, 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
   Link,
   NavLink
 } from 'react-router-dom';
@@ -18,9 +18,9 @@ import './App.css';
 function Navigation(props) {
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark shadow mb-3">
-      <Link className="navbar-brand" to="/">noYOUchoose</Link>
+      <Link className="navbar-brand" to="/">[noYOUchoose]</Link>
       <ul className="navbar-nav mr-auto">
-       
+
         <li className="nav-item">
           <NavLink className="nav-link" exact to="/restaurants">
             Adventures
@@ -36,7 +36,7 @@ function Navigation(props) {
             User *incoming*
           </NavLink>
         </li>
-       
+
       </ul>
     </nav>
   );
@@ -46,20 +46,20 @@ function Navigation(props) {
 class App extends React.Component {
   render() {
     return (
-        <Router>
-          <Navigation />
-          <div className="container-fluid text-center">
-            <div className="row justify-content-center">
-              <Switch>
-                <Route path="/posts/new" component={PostFormPage} />
-                <Route path="/posts/:id" component={ShowPostPage} />
-                <Route path="/restaurants" component={RestaurantsPage} />
-                <Route path="/about-us" component={AboutUsPage} />
-                <Route path="/" component={PostsListPage} />
-              </Switch>
-            </div>
+      <Router>
+        <Navigation />
+        <div className="container-fluid text-center">
+          <div className="row justify-content-center">
+            <Switch>
+              <Route path="/posts/new" component={PostFormPage} />
+              <Route path="/posts/:id" component={ShowPostPage} />
+              <Route path="/restaurants" component={RestaurantsPage} />
+              <Route path="/about-us" component={AboutUsPage} />
+              <Route path="/" component={PostsListPage} />
+            </Switch>
           </div>
-        </Router>
+        </div>
+      </Router>
     );
   }
 }
