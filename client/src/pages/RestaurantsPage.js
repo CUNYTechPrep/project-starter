2dfda0dbedfefb5d26cb0a28796b4aae92805d19
Julio Juarez <juliojuar1693@gmail.com>
2019-12-02 18:57:48 -0500

    Added restaurant page, shows list of restaurants around users location along with its rating & picture. reactstrap & bootstrap install required for cards in jsx

--- client/src/pages/RestaurantsPage.js
+++ client/src/pages/RestaurantsPage.js
@@ -1,9 +1,64 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import {
+  Row, Col, Card, CardBody, CardTitle, CardText, CardImg
+} from 'reactstrap';
 
-function RestaurantsPage(props) {
+
+class RestaurantsPage extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      'items':[]
+    }
+  }
+  getLocation(){
+
+  }
+  componentDidMount(){
+    this.getRes();
+  }
+
+getRes(){
+const lat = sessionStorage.getItem("lat");
+const lng = sessionStorage.getItem("lng");
+fetch('/api/places?lat='+lat+'&lng='+lng+'&keyword=bar')
+  .then(res => res.json())
+  .then(res => this.setState({'items':res.json.results}));
+  }
+render() {
+
   return (
-    <div>List of Restaurants</div>
-  );
+    <ul>
+    {this.state.items.map(function(item, index) {
+        return (
+        <ContentItem item = {item} key={index} />
+
+    )
+    })
+    }
+    </ul>);
+ }
+
 }
+const ContentItem = ({item}) => (
+
+   <Row>
+     <Col xs="3" />
+     <Col xs="12" sm="6">
+       <Card>
+         <CardBody>
+           <CardTitle>
+             <CardImg top width="100%" src={"https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference="+item.photos[0].photo_reference+"&key=________________"}></CardImg>
+             {item.name}
+           </CardTitle>
+           <CardText>
+             {"Rating: "+item.rating}
+           </CardText>
+         </CardBody>
+       </Card>
+     </Col>
+   </Row>
+  )
 
 export default RestaurantsPage;
