8f90de4925acbd542d704efa93221f210abc04ae
Rehman Arshad <rehman.arshad777@gmail.com>
2019-12-06 17:39:57 -0500

    minor tweaks

--- client/src/App.js
+++ client/src/App.js
@@ -35,8 +35,7 @@ class App extends React.Component {
                 {/*  */}
                 {/* <Route path="/posts/new" component={PostFormPage} /> */}
                 {/* <Route path="/posts/:id" component={ShowPostPage} /> */}
-
-                <Route path="/" component={LoginPage}/>
+                
                 <Route path="/login" component={LoginPage} />
                 <Route path="/signup" component={SignUpPage} />
                 <Route path="/dashboard" component={DashboardPage} />
