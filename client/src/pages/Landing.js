bbde2f44e34aa9b735380b721c580dab3764491e
Ellie <j.chen.ellie@gmail.com>
2019-12-13 14:12:24 -0500

    Testing scanner

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -7,6 +7,11 @@ import { NavLink } from "react-router-dom";
 import { FaCheck } from "react-icons/fa";
 import AllergenItems from "./AllergenItems";
 import './SearchBar.css';
+import ScanditBarcodeScanner from "scandit-sdk-react";
+import {
+  Barcode,
+  ScanSettings
+} from "scandit-sdk";
 
 
 /**
@@ -68,6 +73,21 @@ class Landing extends React.Component {
     });
   }
 
+  /* scanner functions */
+
+  toggleScanner = () => {
+    this.setState({
+      scannerStatus: !this.state.scannerStatus
+    })
+  }
+
+  getScanSettings = () => {
+    return new ScanSettings({ enabledSymbologies: [Barcode.Symbology.EAN13, Barcode.Symbology.EAN8] });
+  };
+
+
+  /* product search functions */
+
   productSearch = async (searchQuery) => {
     await fetch("http://45.63.21.86:8000/api/search",
       {
@@ -88,7 +108,7 @@ class Landing extends React.Component {
 
   productCheck = async (productId) => {
     let result;
-    
+
     //Workaround: build array from objects
     let allergies = [];
     this.state.items.forEach((element) => {
@@ -130,7 +150,7 @@ class Landing extends React.Component {
       if (this.state.items < 1) {
         this.searchClicked = false;
         return <div>
-          <br/> {/*workaround */}
+          <br /> {/*workaround */}
           Please enter at least 1 allergy
           </div>
       }
@@ -143,7 +163,7 @@ class Landing extends React.Component {
       } else {
         this.searchClicked = false;
         return <div>
-          <br/> {/*workaround */}
+          <br /> {/*workaround */}
           No results found!
         </div>
       }
@@ -151,9 +171,30 @@ class Landing extends React.Component {
   }
 
   render() {
-    return (
-      <div className="container">
-        {
+    if (this.state.scannerStatus) {
+      return <ScanditBarcodeScanner
+        // Library licensing & configuration options (see https://docs.scandit.com/stable/web/globals.html#configure)
+        licenseKey="AZUdGjVmK4B3QucR/ioDoeoESgdrQYvEM0oVOctvisziSEfBW32fhQ8TQNPSaRI54WjQKYAyT7TQKFnKUxDYwRBKB6zBRWZeM0NK17BkSueSMoKTjT8t5IMpk/Elw7cIz2saWO16PiiyqcDa5BO1ZgS3WpIGH+1CUIrtOOsesNcPDcPv4Vx+vlOeYS5uoDC71DniZK7EJp5KVWWnNNAdCeKLnnbYMCFoKRGuDT3VSNqBc8H/g026YdKif4nTRti6LVS0Bc26Ck6Ci3LcKrpSdBG/vaBRrQapNb77ArPgemasOpT2JiNszvo1hUUuCeenXHDd3f8m9ZPLxmY/XKScbrBJTvavj1kAJth01T9E6V++lENmBlN+WEqGvBV2OgPA7hH3v06C7xJ+nxKtWV0ocYgQysmDlH1U2hFGEwcwEOplGYpbu/Sl6n8M+/KaNup9yBtx0EV5bPYPcpForjd5D8KFsbLR2RG9gLIc37/y4trstamDY4//imAGqOzQkat3E7mD7lNRrJstxeAue9EYugopTIFo3dJza8LXBvmYK52NCoPTftxEBqm/fEvlyV9bzzVD7SpYn9P60cNTMFVIcpxt2o6ESRrtVkloj/+Rkbowl9pZCy6PSEcrEigns06NRWnSOi7ZB2+j1jG0+Ru0H44Xmd1ZPhWmQgHZmkfwjudNHj279uHlLFD5T3Ss0IQs8xSPDb16PxW35b7DbjEju8qscNchtXNZTQ+2XQBjCqSojgTpROlcm+9KulQxEm54Cb+YgWfn6lzenrW9EDgs6tnCn7A2PTQG1jT3"
+        engineLocation="https://cdn.jsdelivr.net/npm/scandit-sdk@4.x/build" // could also be a local folder, e.g. "build"
+
+        // Picker events
+        onReady={() => this.setState({ scannerReady: true })}
+        onScan={(e) => {
+          this.toggleScanner();
+          this.setState({
+            searchValue: e.barcodes[0].data
+          });
+          console.log(this.state.scanUPC);
+        }}
+        onScanError={console.log}
+        //lol
+        // Picker options
+        scanSettings={this.getScanSettings()}
+      />
+    } else {
+      return (
+        <div className="container">
+          {
         /*
             
             <SearchResult img={example} brand="Unbranded" name="Apple" check={true}/>
@@ -164,93 +205,95 @@ class Landing extends React.Component {
 
 
             */}
-        <div className="row justify-content-center title">
-          <h1>Check your Product</h1>
-        </div>
-        <div class="row justify-content-center">
-          <div class="col-sm-6 col-md-6 col-lg-5 offset-md-4 ml-0 mt-3 border border-success pt-3 search">
-            <div class="input-group mb-3">
-              <input type="text" class="form-control" placeholder="Product..." onChange={
-                (e) => {
-                  this.setState({
-                    searchValue: e.target.value
-                  })
-                  console.log(this.state.searchValue);
-                }
-              }></input>
-            </div>
-          </div>
-        </div>
-        <div className="mt-4 allergenList">
-          <div className="row justify-content-center">
-            <div className="header">
-              <form onSubmit={this.addItem}>
-                <input className="border border-success" ref={(a) => this._inputElement = a} placeholder="List Allergen"></input>
-                <button className="border border-success btn btn-dark" type="submit">add</button>
-              </form>
-            </div>
+          <div className="row justify-content-center title">
+            <h1>Check your Product</h1>
           </div>
           <div class="row justify-content-center">
-            <AllergenItems entries={this.state.items} delete={this.deleteItem} />
-          </div>
-          <div id="check">
-            {/*<NavLink exact to="/fuzzy-search"><span class="input-group-text bg-dark">Check <FaCheck style={{color:"#008000"}}/></span></NavLink> */}
-            <button type="button" class="input-group-text bg-dark" onClick={(e) => {
-              this.searchClicked = true;
-              this.productSearch(this.state.searchValue);
-            }}>Check <FaCheck style={{ color: "#008000" }} /></button>
-
-
-          {/* data-toggle="modal" data-target="#good" */}
-        </div>
-      </div>
-
-        { this.printSearchResult() }
-
-    {/*WARNING Modal*/ }
-    <div class="modal fade" id="warning" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-      <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h1 class="modal-title w-100 text-center" id="exampleModalLongTitle">Warning</h1>
-          </div>
-          <div class="modal-body">
-            <div style={this.styleImg}>
-              <img src={warning} alt="WARNING" width={150} height={150} />
+            <div class="col-sm-6 col-md-6 col-lg-5 offset-md-4 ml-0 mt-3 border border-success pt-3 search">
+              <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="Product..." value={this.state.searchValue} onChange={
+                  (e) => {
+                    this.setState({
+                      searchValue: e.target.value
+                    })
+                    console.log(this.state.searchValue);
+                  }
+                }></input>
+                <button onClick={(e) => {this.toggleScanner()}}>Scan</button>
+              </div>
             </div>
-            <h5 style={this.style} class="placeCenter"> This product will trigger a symptom of yours!</h5>
           </div>
-          <div class="modal-footer">
-            <button type="button" class="justify-content-center btn btn-danger" data-dismiss="modal">Close</button>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    {/*Good Modal*/ }
-    <div class="modal fade" id="good" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-      <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h1 class="modal-title w-100 text-center" id="exampleModalLongTitle">Good To Go!</h1>
-          </div>
-          <div class="modal-body">
-            <div style={this.styleImg}>
-              <img src={good} alt="GOOD" width={150} height={150} />
+          <div className="mt-4 allergenList">
+            <div className="row justify-content-center">
+              <div className="header">
+                <form onSubmit={this.addItem}>
+                  <input className="border border-success" ref={(a) => this._inputElement = a} placeholder="List Allergen"></input>
+                  <button className="border border-success btn btn-dark" type="submit">add</button>
+                </form>
+              </div>
             </div>
-            <h5 style={this.style} class="placeCenter"> This product will not trigger a symptom of yours!</h5>
+            <div class="row justify-content-center">
+              <AllergenItems entries={this.state.items} delete={this.deleteItem} />
+            </div>
+            <div id="check">
+              {/*<NavLink exact to="/fuzzy-search"><span class="input-group-text bg-dark">Check <FaCheck style={{color:"#008000"}}/></span></NavLink> */}
+              <button type="button" class="input-group-text bg-dark" onClick={(e) => {
+                this.searchClicked = true;
+                this.productSearch(this.state.searchValue);
+              }}>Check <FaCheck style={{ color: "#008000" }} /></button>
+
+
+              {/* data-toggle="modal" data-target="#good" */}
+            </div>
           </div>
-          <div class="modal-footer">
-            <button type="button" class="justify-content-center btn btn-success" data-dismiss="modal">Close</button>
+
+          {this.printSearchResult()}
+
+          {/*WARNING Modal*/}
+          <div class="modal fade" id="warning" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h1 class="modal-title w-100 text-center" id="exampleModalLongTitle">Warning</h1>
+                </div>
+                <div class="modal-body">
+                  <div style={this.styleImg}>
+                    <img src={warning} alt="WARNING" width={150} height={150} />
+                  </div>
+                  <h5 style={this.style} class="placeCenter"> This product will trigger a symptom of yours!</h5>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="justify-content-center btn btn-danger" data-dismiss="modal">Close</button>
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    </div>
+
+          {/*Good Modal*/}
+          <div class="modal fade" id="good" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h1 class="modal-title w-100 text-center" id="exampleModalLongTitle">Good To Go!</h1>
+                </div>
+                <div class="modal-body">
+                  <div style={this.styleImg}>
+                    <img src={good} alt="GOOD" width={150} height={150} />
+                  </div>
+                  <h5 style={this.style} class="placeCenter"> This product will not trigger a symptom of yours!</h5>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="justify-content-center btn btn-success" data-dismiss="modal">Close</button>
+                </div>
+              </div>
+            </div>
+          </div>
 
 
-      </div >
+        </div >
 
-    );
+      );
+    }
   }
 }
 
