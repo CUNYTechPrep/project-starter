035286934f2ab3bf7bb0eda9b31521ad86b35b97
Genna Lipkin <genlipkin@gmail.com>
2019-12-13 12:48:28 -0500

    Update Candidate.js

--- client/src/components/Candidate.js
+++ client/src/components/Candidate.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import {
     Card, Button, CardImg, CardTitle, CardText, CardDeck,
+<<<<<<< Updated upstream
     CardSubtitle, CardBody
+=======
+    CardSubtitle, CardBody, CardGroup, CardLink, Row, Col
+>>>>>>> Stashed changes
   } from 'reactstrap';
 
 function Candidate({ fname, email, city, zipcode, resumeURL, isBiz }) {
   return (
    <div>
+<<<<<<< Updated upstream
        <Card>
+=======
+
+<CardDeck>
+<Row sm="10">
+      <Col sm="10">
+       <Card >
+       
+>>>>>>> Stashed changes
            <CardBody>
                <CardTitle>
                    Candidate {fname}
@@ -20,7 +33,18 @@ function Candidate({ fname, email, city, zipcode, resumeURL, isBiz }) {
                    </ul>
                </CardText>
            </CardBody>
+<<<<<<< Updated upstream
        </Card>
+=======
+           <Button>Message {fname}</Button>
+       </Card>
+    </Col>
+    </Row>
+       </CardDeck>
+       <CardDeck></CardDeck> 
+
+     
+>>>>>>> Stashed changes
     </div>
   );
 }
