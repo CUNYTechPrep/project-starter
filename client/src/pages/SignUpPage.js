b7d89de5519b51555d9b3cb13dda05b2ede85aea
Hongnuan Lin <hlin3517@gmail.com>
2020-11-14 16:14:57 -0500

    able to redirect after signup

--- client/src/pages/SignUpPage.js
+++ client/src/pages/SignUpPage.js
@@ -1,67 +1,64 @@
-import React from 'react';
-import Axios from 'axios';
-import './SignUpPage.css';
-import {useForm} from 'react-hook-form';
+import React, { useState } from "react"
+import Axios from "axios"
+import "./SignUpPage.css"
+import { useForm } from "react-hook-form"
 import auth from "../services/auth"
-import {useHistory} from "react-router-dom"
+import { Redirect } from "react-router-dom"
 
 //class SignUpPage extends React.Component
-export default function SignUpPage(){
+export default function SignUpPage() {
+    const [success, setSuccess] = useState(false)
 
-    const { register, handleSubmit, errors } = useForm();
-    const history = useHistory()
-    const onSubmit = (data) =>{
+    const { register, handleSubmit, errors } = useForm()
+    const onSubmit = data => {
         Axios.post("/api/auth/signup", {
             email: data.email,
             password: data.password,
-        }).then((response) =>{
-            if (response.ok) {
-                auth.isAuthenticated = true
-                history.push("/")
-            }
-            console.log(response)
-        });
+        }).then(response => {
+            auth.isAuthenticated = true
+            setSuccess(true)
+        })
     }
 
+    if (success) return <Redirect to="/" />
 
-
-        return(
-            <div className='container'>
-                <h3>Create An Account</h3>
+    return (
+        <div className="container">
+            <h3>Create An Account</h3>
             <form className={"ui form"} onSubmit={handleSubmit(onSubmit)}>
-            <div className="field">
-                <label>Email:</label>
-                <input 
-                    type="text" 
-                    name="email" 
-                    placeholder="Email" 
-                    ref={register({
-                        required: "Email Required",
-                        pattern: {
-                          value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                          message: "invalid email address"
-                        }
-                      })}
-                />
-                {errors.email && <p style={{color: "red"}}>{errors.email.message}</p>}
-
-
-            </div>
-            <div className="field">
-                <label>password:</label>
-                <input 
-                    type="password"
-                    name="password" 
-                    placeholder="Password" 
-                    ref={register({required: "Password Required", minLength: {value: 8, message: 'Too short'}})}/>
-                {errors.password && <p style={{color: "red"}} >{errors.password.message}</p>}
-            </div>
-                <button className={"ui button"} type="submit">Submit</button>
-                
+                <div className="field">
+                    <label>Email:</label>
+                    <input
+                        type="text"
+                        name="email"
+                        placeholder="Email"
+                        ref={register({
+                            required: "Email Required",
+                            pattern: {
+                                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                message: "invalid email address",
+                            },
+                        })}
+                    />
+                    {errors.email && <p style={{ color: "red" }}>{errors.email.message}</p>}
+                </div>
+                <div className="field">
+                    <label>password:</label>
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="Password"
+                        ref={register({
+                            required: "Password Required",
+                            minLength: { value: 8, message: "Too short" },
+                        })}
+                    />
+                    {errors.password && <p style={{ color: "red" }}>{errors.password.message}</p>}
+                </div>
+                <button className={"ui button"} type="submit">
+                    Submit
+                </button>
             </form>
-           
-            </div>
-            );
-
+        </div>
+    )
 }
-
