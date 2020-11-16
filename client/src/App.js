1ae2e97e4341243e174e671440daaa10165771f6
Sett Hein <settnaing199@gmail.com>
2020-11-16 00:18:49 -0500

    path for private route

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import React from "react"
 import { BrowserRouter as Router, Switch, Route, Link, NavLink, Redirect } from "react-router-dom"
 import LandingPage from "./pages/LandingPage"
-
 import LoginPage from "./pages/LoginPage"
 import SignUpPage from "./pages/SignUpPage"
+
+import MessagePage from './pages/MessagePage';
+import MatchPage from './pages/MatchPage';
+import ProfilePage from './pages/ProfilePage';
 import PrivateRoute from "./components/PrivateRoute"
 import { Navigation } from "./components/Navigation"
 import "./App.css"
@@ -21,13 +24,16 @@ class App extends React.Component {
                                 exact
                                 path="/"
                                 render={() =>
-                                    auth.isAuthenticated ? <h1>Home Page</h1> : <LandingPage />
+                                    auth.isAuthenticated ? <div>Hello</div>: <LandingPage />
                                 }
                             />
-                            <Route exact path="/login" component={LoginPage} />
-
+                            <Route exact  path="/login" component={LoginPage} />
                             <Route exact path="/signup" component={SignUpPage} />
                             {/* Force nomatch urls redirect to login page */}
+                            <PrivateRoute  exact path = '/profile' component={ProfilePage} />
+                            <PrivateRoute exact path = '/match' component={MatchPage} />
+                            <PrivateRoute exact path = '/message' component={MessagePage} />
+
                             <Route render={() => <Redirect to="/" />} />
                         </Switch>
                     </div>
