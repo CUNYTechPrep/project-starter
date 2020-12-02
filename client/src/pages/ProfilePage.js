eed5bef994a26fad537872eb41c5a50b761fcf24
Sett Hein <settnaing199@gmail.com>
2020-12-02 14:28:26 -0500

    add picture and dummy data for interests

--- client/src/pages/ProfilePage.js
+++ client/src/pages/ProfilePage.js
@@ -7,7 +7,7 @@ import { Redirect } from "react-router-dom"
 import axios from "axios"
 import auth from "../services/auth" // replace with js-cookie or react-cookie
 import Header3 from "../components/Header3"
-
+import headshot from './Images/profile-picture.png';
 class ProfilePage extends React.Component {
     // get profile from http://localhost:8080/api/profile
 
@@ -32,11 +32,23 @@ class ProfilePage extends React.Component {
         if (this.state.loading) return <Loading />
         return (
             <div className="profile">
-                <Header3
+                <div class="ui grid">
+                <div class="three wide column">
+                <img src={headshot}  style={{width: "100px"}} />
+                </div>
+                <div class="thirteen wide column" style={{marginTop: "10px"}}>
+                    <Header3
                     headerName={` ${this.state.profile.firstName} ${this.state.profile.lastName}`}
-                />
-                <div> {this.state.profile.school} </div>
-                <div> {this.state.profile.major} </div>
+                    />
+                    <div> {this.state.profile.school} </div>
+                    <div style={{color: "#696969"}}> {`Bachelor in ${this.state.profile.major}`} </div>
+                    <div style={{color: "grey"}}> {`Class of ${this.state.profile.graduate_date}`} </div>
+
+
+                </div>
+                
+                </div>
+
                 <hr style={{ borderColor: "rgba(0.1, 0.1, 0, 0.1)" }} />
                 <Header3 headerName="About" />
                 {this.state.profile.bio}
@@ -45,7 +57,8 @@ class ProfilePage extends React.Component {
                 <Tag classes={this.state.profile.coursesTaken.map(course => course.label)} />
                 <hr style={{ borderColor: "rgba(0.1, 0.1, 0, 0.1)" }} />
                 <Header3 headerName="Interests" />
-                <br />
+                <Tag  classes={["Hiking", "Reading", "Investing"]} />
+                <br/>
                 <button onClick={this.handleClick} className="positive ui button">
                     Edit Profile
                 </button>
