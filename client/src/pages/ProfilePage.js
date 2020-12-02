0aee725db0a00c3a3547dd7753dd033d4bcc9152
Sett Hein <settnaing199@gmail.com>
2020-12-01 19:15:18 -0500

    something

--- client/src/pages/ProfilePage.js
+++ client/src/pages/ProfilePage.js
@@ -6,6 +6,7 @@ import Loading from "../components/Loading"
 import { Redirect } from "react-router-dom"
 import axios from "axios"
 import auth from "../services/auth" // replace with js-cookie or react-cookie
+import Header3 from '../components/Header3';
 
 class ProfilePage extends React.Component {
     // get profile from http://localhost:8080/api/profile
@@ -31,15 +32,18 @@ class ProfilePage extends React.Component {
         if (this.state.loading) return <Loading />
         return (
             <div className="profile">
-                <h3>{` ${this.state.profile.firstName} ${this.state.profile.lastName}`}</h3>
-                <p> {this.state.profile.school} </p>
-
-                <p> {this.state.profile.major} </p>
-                <h3>About</h3>
+                <Header3 headerName={` ${this.state.profile.firstName} ${this.state.profile.lastName}`} />
+                <div> {this.state.profile.school} </div>
+                <div> {this.state.profile.major} </div>
+                <hr style={{borderColor: "rgba(0.1, 0.1, 0, 0.1)"}}/>
+                <Header3 headerName="About"/>
                 {this.state.profile.bio}
-
-                <h3>Classes</h3>
+                <hr style={{borderColor: "rgba(0.1, 0.1, 0, 0.1)"}}/>
+                <Header3 headerName="Classes"/>
                 <Tag classes={this.state.profile.coursesTaken} />
+                <hr style={{borderColor: "rgba(0.1, 0.1, 0, 0.1)"}}/>
+                <Header3 headerName="Interests"/>
+                <br />
                 <button onClick={this.handleClick} className="positive ui button">
                     Edit Profile
                 </button>
