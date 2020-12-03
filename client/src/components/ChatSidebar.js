bd18e4c5e05bccaf2d235bbaef99366691f7eb9c
Alex <hlin3517@gmail.com>
2020-12-03 18:59:43 -0500

    Update chat style

--- client/src/components/ChatSidebar.js
+++ client/src/components/ChatSidebar.js
@@ -1,5 +1,22 @@
 import React from "react"
-import { Box, Typography, Tabs, Tab, Grid } from "@material-ui/core"
+import { Box, Typography, Tabs, Tab, Grid, Button } from "@material-ui/core"
+
+import { makeStyles } from "@material-ui/core/styles"
+import List from "@material-ui/core/List"
+import ListItem from "@material-ui/core/ListItem"
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction"
+import ListItemText from "@material-ui/core/ListItemText"
+import ListItemAvatar from "@material-ui/core/ListItemAvatar"
+import Checkbox from "@material-ui/core/Checkbox"
+import Avatar from "@material-ui/core/Avatar"
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        width: "100%",
+        maxWidth: 360,
+        backgroundColor: theme.palette.background.paper,
+    },
+}))
 
 function TabPanel(props) {
     const { children, value, index, ...other } = props
@@ -33,9 +50,24 @@ function ChatSidebar() {
 
             <Grid item>
                 <TabPanel value={value} index={0}>
-                    {[...Array(10).keys()].map(i => (
-                        <div key={i}>{i}</div>
-                    ))}
+                    <List dense>
+                        {[0, 1, 2, 3].map(value => {
+                            const labelId = `checkbox-list-secondary-label-${value}`
+                            return (
+                                <ListItem key={value} button>
+                                    <ListItemAvatar>
+                                        <Avatar alt="H" src={""} />
+                                    </ListItemAvatar>
+                                    <ListItemText id={labelId} primary={`User ${value + 1}`} />
+                                    <ListItemSecondaryAction>
+                                        <Button variant="outlined" color="primary" dark>
+                                            Chat
+                                        </Button>
+                                    </ListItemSecondaryAction>
+                                </ListItem>
+                            )
+                        })}
+                    </List>
                 </TabPanel>
                 <TabPanel value={value} index={1}>
                     Item Two
