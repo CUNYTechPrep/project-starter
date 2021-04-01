1017a5dd1ec2e9eec5d2cdd4e5bc575bb53a2239
Justin Kuang <justin.kuang63@myhunter.cuny.edu>
2021-03-31 20:26:29 -0400

    Add ForumPage.js and missing alt tags
    - Added root ForumPage component
    - Added missing 'alt' tags for dumbell logos on signuo/login pages
    - Redirected nav "Forum" to ForumPage.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { 
-  BrowserRouter as Router, 
-  Switch, 
-  Route, 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
   Link,
-  NavLink
+  NavLink,
 } from 'react-router-dom';
 import PostsListPage from './pages/PostsListPage';
-import PostFormPage from './pages/PostFormPage';
+//import PostFormPage from './pages/PostFormPage';
 import ShowPostPage from './pages/ShowPostPage';
 import AboutUsPage from './pages/AboutUsPage';
+import ForumPage from './pages/ForumPage';
 
 import LoginPage from './pages/LoginPage';
 import PlacesPage from './pages/PlacesPage';
@@ -17,58 +18,57 @@ import PlacesPage from './pages/PlacesPage';
 import './App.css';
 import SignUpPage from './pages/SignUpPage';
 
-
 function Navigation(props) {
   return (
-    <nav className="navbar navbar-default navbar-dark" >
-      <Link className="navbar-brand" to="/">Fitbud</Link>
-        <div className="nav-item">
-          <NavLink className="nav-link" exact to="/posts/new">
-            Forum
-          </NavLink>
-        </div>
-        <div className="nav-item">
-          <NavLink className="nav-link" exact to="/places">
-            Places
-          </NavLink>
-        </div>
-        <div className="nav-item">
-          <NavLink className="nav-link" exact to="/sign-in">
-            Sign In
-          </NavLink>
-        </div>
-        <div className="nav-item">
-          <NavLink className="nav-link" exact to="/about-us">
-            About Us
-          </NavLink>
-        </div>
+    <nav className="navbar navbar-default navbar-dark">
+      <Link className="navbar-brand" to="/">
+        Fitbud
+      </Link>
+      <div className="nav-item">
+        <NavLink className="nav-link" exact to="/forum">
+          Forum
+        </NavLink>
+      </div>
+      <div className="nav-item">
+        <NavLink className="nav-link" exact to="/places">
+          Places
+        </NavLink>
+      </div>
+      <div className="nav-item">
+        <NavLink className="nav-link" exact to="/sign-in">
+          Sign In
+        </NavLink>
+      </div>
+      <div className="nav-item">
+        <NavLink className="nav-link" exact to="/about-us">
+          About Us
+        </NavLink>
+      </div>
     </nav>
   );
 }
 
-
 class App extends React.Component {
   render() {
     return (
-        <Router>
-          <Navigation />
-          <div className="container-fluid text-center">
-            <div className="row justify-content-center">
-              <Switch>
-                <Route path="/posts/new" component={PostFormPage} />
-                <Route path="/posts/:id" component={ShowPostPage} />
-                <Route path="/places" component={PlacesPage} />
-                <Route path="/about-us" component={AboutUsPage} />
-                <Route path="/sign-in" component={LoginPage} />
-                <Route path="/sign-up" component={SignUpPage} />
-                <Route path="/" component={PostsListPage} />
-              </Switch>
-            </div>
+      <Router>
+        <Navigation />
+        <div className="container-fluid text-center">
+          <div className="row justify-content-center">
+            <Switch>
+              <Route path="/forum" component={ForumPage} />
+              <Route path="/posts/:id" component={ShowPostPage} />
+              <Route path="/places" component={PlacesPage} />
+              <Route path="/about-us" component={AboutUsPage} />
+              <Route path="/sign-in" component={LoginPage} />
+              <Route path="/sign-up" component={SignUpPage} />
+              <Route path="/" component={PostsListPage} />
+            </Switch>
           </div>
-        </Router>
+        </div>
+      </Router>
     );
   }
 }
-
 
 export default App;
---
+++ client/src/pages/ForumPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ForumPage(props) {
+  return (
+    <div>
+      <h1>Hello Forum</h1>
+    </div>
+  );
+}
+
+export default ForumPage;
