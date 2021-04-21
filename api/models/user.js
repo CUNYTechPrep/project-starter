0ad17a380ec2826d68280107d4d46c3944ff6fe2
Shanice Smith <shanicesmith.work@hotmail.com>
2021-04-21 16:53:38 -0400

    created User and Group db models

---
+++ api/models/user.js
@@ -0,0 +1,43 @@
+'use strict';
+const { Model } = require('sequelize');
+
+
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {}
+
+  User.init({
+    userID: {
+      type: DataTypes.STRING,
+      validate: {
+        len: [3, 250],
+        notEmpty: true,
+      }
+    },
+    emailAddress: {
+        type: DataTypes.STRING,
+        validate: {
+            len: [5, 100],
+            notEmpty: true,
+        }
+    },
+    groups: {
+        type: DataTypes.ARRAY,
+    },
+    password: {
+        type: DataTypes.STRING,
+        validate: {
+            len: [8, 100],
+            notEmpty: true,
+        }
+    },
+  }, {
+    sequelize,
+    modelName: 'user'
+  });
+
+  User.associate = (models) => {
+    // associations can be defined here
+  };
+
+  return User;
+};
