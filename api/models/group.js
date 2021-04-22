e8930a7101d8418beafb528f56c24a94b8b2a93c
Jephter Maurice <mjephter2@gmail.com>
2021-04-22 15:38:37 -0400

    commented out codes to allow program to run error free

--- api/models/group.js
+++ api/models/group.js
@@ -1,35 +1,37 @@
-'use strict';
-const { Model } = require('sequelize');
-
+"use strict";
+const { Model } = require("sequelize");
 
 module.exports = (sequelize, DataTypes) => {
   class Group extends Model {}
 
-  Group.init({
-    groupID: {
-      type: DataTypes.STRING,
-      validate: {
-        len: [8],
-        notEmpty: true,
-      }
-    },
-    members: {
-        type: DataTypes.ARRAY,
+  Group.init(
+    {
+      groupID: {
+        type: DataTypes.STRING,
         validate: {
-            len: [1, 8],
-            notEmpty: true,
-        }
+          len: [8],
+          notEmpty: true,
+        },
+      },
+      // members: {
+      //     type: DataTypes.ARRAY,
+      //     validate: {
+      //         len: [1, 8],
+      //         notEmpty: true,
+      //     }
+      // },
+      eventDate: {
+        type: DataTypes.DATE,
+      },
+      // places: {
+      //     type: DataTypes.ARRAY
+      // },
     },
-    eventDate: {
-        type: DataTypes.DATE
-    },
-    places: {
-        type: DataTypes.ARRAY
-    },
-  }, {
-    sequelize,
-    modelName: 'group'
-  });
+    {
+      sequelize,
+      modelName: "group",
+    }
+  );
 
   Group.associate = (models) => {
     // associations can be defined here
--- api/models/user.js
+++ api/models/user.js
@@ -1,39 +1,41 @@
-'use strict';
-const { Model } = require('sequelize');
-
+"use strict";
+const { Model } = require("sequelize");
 
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {}
 
-  User.init({
-    userID: {
-      type: DataTypes.STRING,
-      validate: {
-        len: [3, 250],
-        notEmpty: true,
-      }
-    },
-    emailAddress: {
+  User.init(
+    {
+      userID: {
         type: DataTypes.STRING,
         validate: {
-            len: [5, 100],
-            notEmpty: true,
-        }
-    },
-    groups: {
-        type: DataTypes.ARRAY,
-    },
-    password: {
+          len: [3, 250],
+          notEmpty: true,
+        },
+      },
+      emailAddress: {
         type: DataTypes.STRING,
         validate: {
-            len: [8, 100],
-            notEmpty: true,
-        }
+          len: [5, 100],
+          notEmpty: true,
+        },
+      },
+      // groups: {
+      //     type: DataTypes.ARRAY,
+      // },
+      password: {
+        type: DataTypes.STRING,
+        validate: {
+          len: [8, 100],
+          notEmpty: true,
+        },
+      },
     },
-  }, {
-    sequelize,
-    modelName: 'user'
-  });
+    {
+      sequelize,
+      modelName: "user",
+    }
+  );
 
   User.associate = (models) => {
     // associations can be defined here
