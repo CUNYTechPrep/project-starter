43adcad35071af677cdb4550d855f21d0f74d217
Jephter Maurice <mjephter2@gmail.com>
2021-05-02 16:07:45 -0400

    added model Group

---
+++ api/models/group.js
@@ -0,0 +1,40 @@
+"use strict";
+const { Model } = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+  class Group extends Model {}
+
+  Group.init(
+    {
+      groupId: {
+        type: DataTypes.STRING,
+        validate: {
+          len: [10, 20],
+          notEmpty: true,
+        },
+        unique: true,
+      },
+      groupName: {
+        type: DataTypes.STRING,
+        defaultValue: "UnNamedGroup",
+      },
+      members: {
+        type: DataTypes.ARRAY(DataTypes.STRING),
+      },
+      places: {
+        type: DataTypes.ARRAY(DataTypes.STRING),
+      },
+    },
+    {
+      sequelize,
+      modelName: "group",
+    }
+  );
+
+  Group.associate = (models) => {
+    // associations can be defined here
+    //models.Group.belongsToMany(models.User, { through: "GroupUser" });
+  };
+
+  return Group;
+};
