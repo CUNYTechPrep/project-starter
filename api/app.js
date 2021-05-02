1f0080fd071c8e26b02f1676148e5a36fba10c49
Jephter Maurice <mjephter2@gmail.com>
2021-05-02 16:13:54 -0400

    incorporated seed file

--- api/app.js
+++ api/app.js
@@ -1,28 +1,30 @@
-const express = require('express');
-const morgan = require('morgan');
-const path = require('path');
-const db = require('./models');
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+const db = require("./models");
 const app = express();
 const PORT = process.env.PORT;
+const seed = require("./seed");
 
+seed();
 
 // this lets us parse 'application/json' content in http requests
 app.use(express.json());
 
 // add http request logging to help us debug and audit app use
-const logFormat = process.env.NODE_ENV==='production' ? 'combined' : 'dev';
+const logFormat = process.env.NODE_ENV === "production" ? "combined" : "dev";
 app.use(morgan(logFormat));
 
 // this mounts controllers/index.js at the route `/api`
-app.use('/api', require('./controllers'));
+app.use("/api", require("./controllers"));
 
 // for production use, we serve the static react build folder
-if(process.env.NODE_ENV==='production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 
   // all unknown routes should be handed to our react app
-  app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
   });
 }
 
@@ -31,8 +33,8 @@ if(process.env.NODE_ENV==='production') {
 db.sequelize.sync({ force: false });
 
 // start up the server
-if(PORT) {
+if (PORT) {
   app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 } else {
-  console.log("===== ERROR ====\nCREATE A .env FILE!\n===== /ERROR ====")
+  console.log("===== ERROR ====\nCREATE A .env FILE!\n===== /ERROR ====");
 }
