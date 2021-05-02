d5961dc87adb572c9a75b7fc5d6c94c77138e76c
Jephter Maurice <mjephter2@gmail.com>
2021-05-02 16:15:25 -0400

    added routings for /places and /groups

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -6,12 +6,17 @@ const postsController = require("./posts.js");
 const appConfigController = require("./appConfig.js");
 
 const placesController = require("./places.js");
+const usersController = require("./users");
+const groupsController = require("./groups");
 
 // Mount each controller under a specific route. These
 // will be prefixes to all routes defined inside the controller
 router.use("/posts", postsController);
 router.use("/application-configuration", appConfigController);
 
+router.use("/places", placesController);
+router.use("/users", usersController);
+router.use("/groups", groupsController);
 router.use("/places", placesController);
 
 module.exports = router;
