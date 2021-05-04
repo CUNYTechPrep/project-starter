268feec467b164a0e7c19749e2611cac6ab4aae2
Jephter Maurice <mjephter2@gmail.com>
2021-05-04 18:17:37 -0400

    added boiler plate for a group creation page

---
+++ client/src/pages/GroupCreationPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class GroupCreationPage extends React.Component {
+  state = {};
+
+  render() {
+    return <h1>GroupCreationPage</h1>;
+  }
+}
+
+export default GroupCreationPage;
