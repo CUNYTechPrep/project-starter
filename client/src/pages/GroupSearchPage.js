c968bbe76bd66370a13535f3e2591f99eead6743
Jephter Maurice <mjephter2@gmail.com>
2021-05-04 18:18:52 -0400

    added boiler plate for a group search page

---
+++ client/src/pages/GroupSearchPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class GroupSearchPage extends React.Component {
+  state = {};
+
+  render() {
+    return <h1>GroupSearchPage</h1>;
+  }
+}
+
+export default GroupSearchPage;
