e13b092f46b2f2b5315d42b1e2fd13fff163ec12
lindaerin <lli007@citymail.cuny.edu>
2021-05-05 18:10:37 -0400

    fix compare issue

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -30,7 +30,7 @@ export default function Login() {
     <div>
       <Container
         className="d-flex align-items-center justify-content-center"
-        style={{ minHeight: "90vh", minWidth: "200vw" }}
+        style={{ minHeight: "90vh" }}
       >
         <div className="w-100" style={{ maxWidth: "400px" }}>
           <Card>
