29db278b70700df2ffe7f39201fca85f5e28fec6
jpho8799 <josephine.pho8799@gmail.com>
2021-05-06 15:54:45 -0400

    bookmark controller and model

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -5,13 +5,13 @@ const router = express.Router();
 // Load each controller
 const postsController = require('./posts.js');
 const appConfigController = require('./appConfig.js');
-const reviewController = require('./review.js');
+const bookmarkController = require('./bookmarks.js');
 
 // Mount each controller under a specific route. These
 // will be prefixes to all routes defined inside the controller
 router.use('/posts', postsController);
 router.use('/application-configuration', appConfigController);
-router.use('/review', reviewController);
+router.use('/bookmarks', bookmarkController);
 
 
 module.exports = router;
---
+++ api/models/bookmarks.js
@@ -0,0 +1,31 @@
+'use strict';
+const {Model} = require ('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+    class Bookmark extends Model {}
+
+    Bookmark.init({
+        userUUID: {
+            type: DataTypes.STRING,
+            validate: {
+                notEmpty: true,
+              
+                
+            }
+        },
+        schoolDBID: {
+            type: DataTypes.STRING, 
+            validate: {
+                notEmpty: true,
+               
+            }
+        }
+        
+    
+    }, {
+        sequelize, 
+        modelName: 'bookmark'
+    });
+
+    return Bookmark;
+}
