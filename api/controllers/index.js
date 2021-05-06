dcac672db555fbb3d3b2c16bc2b8ca1b90a9c088
jpho8799 <josephine.pho8799@gmail.com>
2021-05-06 18:31:50 -0400

    add missing code

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -6,12 +6,14 @@ const router = express.Router();
 const postsController = require('./posts.js');
 const appConfigController = require('./appConfig.js');
 const bookmarkController = require('./bookmarks.js');
+const reviewController = require('./review.js');
 
 // Mount each controller under a specific route. These
 // will be prefixes to all routes defined inside the controller
 router.use('/posts', postsController);
 router.use('/application-configuration', appConfigController);
 router.use('/bookmarks', bookmarkController);
+router.use('/review', reviewController);
 
 
 module.exports = router;
