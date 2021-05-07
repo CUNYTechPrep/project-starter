985bd152ed3fefca76abb157ce888bb721e5dc07
Henry-Cevallos <henry.cevallos95@myhunter.cuny.edu>
2021-05-07 13:10:59 -0400

    did footer

---
+++ client/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+function Footer(props) {
+
+    return (
+
+        <footer className="fixed-bottom bg-dark">
+            <div class="footer-copyright text-center text-white py-3">© 2021 Copyright
+                <p>PDF Media</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
