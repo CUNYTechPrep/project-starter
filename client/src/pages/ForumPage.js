c7d81e7e22fb277b351f9a1fdbb4e216c5cc94ae
Farhene Sultana <farhenesultana@gmail.com>
2021-05-16 18:48:50 -0400

    fixed up forum, will work on Post method later

--- client/src/pages/ForumPage.js
+++ client/src/pages/ForumPage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ThreadStarterTable from '../components/Forum/ForumStarterTable';
-import ThreadCategoryTable from '../components/Forum/ForumCategoryTable';
 import '../css/ForumCategoryTable.css';
 
 function ForumPage(props) {
+
+
+  //do post method here to the FORUM table!
+
   return (
     <div style={{ marginBottom: 40 }}>
-      <ThreadCategoryTable />
       <button className="new-thread-button">New Thread</button>
       <ThreadStarterTable />
     </div>
