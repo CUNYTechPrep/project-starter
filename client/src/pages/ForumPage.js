37111027d54b1d0b46c8fd845d6e0ec5d456a764
Farhene Sultana <farhenesultana@gmail.com>
2021-05-16 19:04:01 -0400

    adding new Thread button and fields to the forum on Justin Kuang's work

--- client/src/pages/ForumPage.js
+++ client/src/pages/ForumPage.js
@@ -8,8 +8,27 @@ function ForumPage(props) {
   //do post method here to the FORUM table!
 
   return (
-    <div style={{ marginBottom: 40 }}>
-      <button className="new-thread-button">New Thread</button>
+    <div className="div-heading">
+      <form>
+        <div className="thread-create">
+          <input 
+            placeholder="Enter Thread Title"
+            maxLength="70"
+            minLength="3"
+            className="thread-new"
+            />
+          <input 
+            placeholder="Enter Thread Category"
+            maxLength="20"
+            minLength="3"
+            className="thread-new"
+          />
+          <button className="new-thread-button" type="submit"> 
+            Post New Thread
+          </button>
+        </div>
+      </form>
+
       <ThreadStarterTable />
     </div>
   );
