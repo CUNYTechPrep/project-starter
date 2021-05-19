9e67bcaf75e817cecc5c1dfb045d1bcff2bd2af7
Jephter Maurice <mjephter2@gmail.com>
2021-05-19 18:19:07 -0400

    added routings for remaining pages and removed navigation bar App.js

--- client/src/App.js
+++ client/src/App.js
@@ -8,17 +8,23 @@ import {
 } from "react-router-dom";
 import ProfilePage from "./pages/ProfilePage";
 import LandingPage from "./pages/LandingPage";
-import NavBar from "../src/components/NavBar";
-
+import GroupSearchPage from "./pages/GroupSearchPage";
+import AboutUsPage from "./pages/AboutUsPage";
+import SignUpPage from "./pages/SignUpPage";
 import "./App.css";
+import GroupCreationPage from "../src/pages/GroupCreationPage";
 
 class App extends React.Component {
   render() {
     return (
       <Router>
-        <NavBar />
+        {/* <NavBar /> */}
         <div>
           <Switch>
+            <Route path="/aboutUs" component={AboutUsPage} />
+            <Route path="/signUp" component={SignUpPage} />
+            <Route path="/searchJoin/:email" component={GroupSearchPage} />
+            <Route path="/createGroup/:email" component={GroupCreationPage} />
             <Route path="/users/:email" component={ProfilePage} />
             <Route path="/" component={LandingPage} />
           </Switch>
