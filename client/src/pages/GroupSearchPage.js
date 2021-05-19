941253b7493a8bc38b13016966a09ff7c88e292e
Jephter Maurice <mjephter2@gmail.com>
2021-05-19 18:26:23 -0400

    added group search page boiler plate

--- client/src/pages/GroupSearchPage.js
+++ client/src/pages/GroupSearchPage.js
@@ -1,10 +1,23 @@
 import React from "react";
 
 class GroupSearchPage extends React.Component {
-  state = {};
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+    };
+  }
+  componentDidMount() {
+    const { email } = this.props.match.params;
+    this.setState({
+      email: email,
+    });
+  }
 
   render() {
-    return <h1>GroupSearchPage</h1>;
+    return (
+      <h1 style={{ color: "white" }}>GroupSearchPage for {this.state.email}</h1>
+    );
   }
 }
 
