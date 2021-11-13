10f61bb3802f1317778b285519a326348a7545eb
Edgardo Molina <medgardo@gmail.com>
2021-11-12 19:25:45 -0500

    (auth) backend: add auth controller

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -5,9 +5,11 @@ const router = express.Router();
 // Load each controller
 const postsController = require('./posts.js');
 const appConfigController = require('./appConfig.js');
+const authController = require('./auth.js');
 
 // Mount each controller under a specific route. These
 // will be prefixes to all routes defined inside the controller
+router.use('/auth', authController);
 router.use('/posts', postsController);
 router.use('/application-configuration', appConfigController);
 
