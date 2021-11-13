a9c695f4d7bf0aed5fb0d25b420ba3356e4523eb
Edgardo Molina <medgardo@gmail.com>
2021-11-12 20:38:42 -0500

    (auth) frontend: check auth state on refresh

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -1,4 +1,4 @@
-import React, { useState, createContext } from 'react'
+import React, { useState, useEffect, createContext } from 'react'
 
 const AuthContext = createContext();
 const { Provider } = AuthContext;
@@ -6,6 +6,19 @@ const { Provider } = AuthContext;
 
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(false)
+
+  useEffect(() => {
+    fetch('/api/auth/login')
+      .then(response => {
+        if(!response.ok) {
+          throw new Error('Unauthenticated')
+        }
+
+        return response.json();
+      })
+      .then(body => setUser(body))
+      .catch(err => setUser(false))
+  }, [])
 
   const authenticate = (email, password) => {
     return fetch('/api/auth/login', {
