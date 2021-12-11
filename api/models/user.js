4bc6d1751f1405eee2f9c76e1b8db70b4cccca9c
Leah <benteriordesign@gmail.com>
2021-12-11 11:33:09 -0500

    Fixing merge conflict

--- api/models/user.js
+++ api/models/user.js
@@ -1,45 +1,66 @@
+'use strict';
 const { Model } = require('sequelize');
-const bcrypt = require('bcryptjs');
 
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    getFullname() {
-      return [this.firstName, this.lastName].join(' ');
-    }
-  }
+  class User extends Model {}
 
   User.init({
-    firstName: { type: DataTypes.STRING },
-    lastName: { type: DataTypes.STRING },
-    email: {
+    username: {
       type: DataTypes.STRING,
+      validate: {
+        len: [5, 50],
+        notEmpty: true,
+      },
       unique: true,
-      allowNull: false,
-      validate: {
-        isEmail: true,
-      },
     },
-    passwordHash: { type: DataTypes.STRING },
-    password: {
-      type: DataTypes.VIRTUAL,
-      validate: {
-        isLongEnough: (val) => {
-          if (val.length < 7) {
-            throw new Error("Please choose a longer password");
-          }
+    firstName: {
+        type: DataTypes.STRING,
+        validate: {
+          len: [2, 50],
+          notEmpty: true,
         },
-      },
+    },
+    lastName: {
+        type: DataTypes.STRING,
+        validate: {
+            len: [2, 50],
+            notEmpty: true,
+        },
+    },
+    dob: {
+        type: DataTypes.DATEONLY,
+    },
+    gender: {
+        type: DataTypes.STRING,
+    },
+    email: {
+        type: DataTypes.STRING,
+        validate: {
+            len: [5, 50],
+            notEmpty: true,
+        },
+        unique: true,
+    },
+    password: {
+        type: DataTypes.STRING,
+        validate: {
+            len: [5, 50],
+            notEmpty: true,
+        }
     },
   }, {
     sequelize,
     modelName: 'user'
-  })
-
-  User.beforeSave((user, options) => {
-    if(user.password) {
-      user.passwordHash = bcrypt.hashSync(user.password, 10);
-    }
   });
 
-return User;
-}
+  User.associate = (models) => {
+    // associations can be defined here
+
+    models.User.hasMany(models.Message, {as: 'Sender', foreignKey: 'senderId'});
+    models.User.hasMany(models.Message, {as: 'Recipient', foreignKey: 'recipientId'});
+    models.User.hasMany(models.UserMatch, {as: 'CurrentUser', foreignKey: 'userId'});
+    models.User.hasMany(models.UserMatch, {as: 'OtherUser', foreignKey: 'matchId'});
+  };
+
+  return User;
+};
