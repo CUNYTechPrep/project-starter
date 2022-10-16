3f4b58ed90d48dfec87042a60bb0e0733ef9ab79
Edgardo Molina <medgardo@gmail.com>
2022-10-16 12:00:47 -0400

    Apply prettier to the client app

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,24 @@
-import React from 'react';
-import { 
-  BrowserRouter as Router, 
-  Switch, 
-  Route, 
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
   Link,
-  NavLink
-} from 'react-router-dom';
-import PostsListPage from './pages/PostsListPage';
-import PostFormPage from './pages/PostFormPage';
-import ShowPostPage from './pages/ShowPostPage';
-import AboutUsPage from './pages/AboutUsPage';
+  NavLink,
+} from "react-router-dom";
+import PostsListPage from "./pages/PostsListPage";
+import PostFormPage from "./pages/PostFormPage";
+import ShowPostPage from "./pages/ShowPostPage";
+import AboutUsPage from "./pages/AboutUsPage";
 
-import './App.css';
-
+import "./App.css";
 
 function Navigation(props) {
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark shadow mb-3">
-      <Link className="navbar-brand" to="/">Micro Blog</Link>
+      <Link className="navbar-brand" to="/">
+        Micro Blog
+      </Link>
       <ul className="navbar-nav mr-auto">
         <li className="nav-item">
           <NavLink className="nav-link" exact to="/posts/new">
@@ -34,26 +35,24 @@ function Navigation(props) {
   );
 }
 
-
 class App extends React.Component {
   render() {
     return (
-        <Router>
-          <Navigation />
-          <div className="container-fluid text-center">
-            <div className="row justify-content-center">
-              <Switch>
-                <Route path="/posts/new" component={PostFormPage} />
-                <Route path="/posts/:id" component={ShowPostPage} />
-                <Route path="/about-us" component={AboutUsPage} />
-                <Route path="/" component={PostsListPage} />
-              </Switch>
-            </div>
+      <Router>
+        <Navigation />
+        <div className="container-fluid text-center">
+          <div className="row justify-content-center">
+            <Switch>
+              <Route path="/posts/new" component={PostFormPage} />
+              <Route path="/posts/:id" component={ShowPostPage} />
+              <Route path="/about-us" component={AboutUsPage} />
+              <Route path="/" component={PostsListPage} />
+            </Switch>
           </div>
-        </Router>
+        </div>
+      </Router>
     );
   }
 }
-
 
 export default App;
--- client/src/pages/AboutUsPage.js
+++ client/src/pages/AboutUsPage.js
@@ -1,9 +1,7 @@
-import React from 'react';
+import React from "react";
 
 function AboutUsPage(props) {
-  return (
-    <div>A little info about us.</div>
-  );
+  return <div>A little info about us.</div>;
 }
 
 export default AboutUsPage;
