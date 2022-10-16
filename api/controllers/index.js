da56881214db817283c45ea550e705e97f1e7e2d
Edgardo Molina <medgardo@gmail.com>
2022-10-16 00:56:30 -0400

    Refactor controllers for clarity
    - rename posts controller microPosts
    - use refactored MicroPost model
    - rename url route from /posts to /micro-posts
    - remove appConfig controller to avoid confusion

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -2,12 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 // Load each controller
-const postsController = require("./posts.js");
-const appConfigController = require("./appConfig.js");
+const microPostsController = require("./microPosts.js");
 
 // Mount each controller under a specific route. These
 // will be prefixes to all routes defined inside the controller
-router.use("/posts", postsController);
-router.use("/application-configuration", appConfigController);
+router.use("/micro_posts", microPostsController);
 
 module.exports = router;
--- api/controllers/microPosts.js
+++ api/controllers/microPosts.js
@@ -1,30 +1,26 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../models");
-const { Post } = db;
+const { MicroPost } = db;
 
 // This is a simple example for providing basic CRUD routes for
 // a resource/model. It provides the following:
-//    GET    /posts
-//    POST   /posts
-//    GET    /posts/:id
-//    PUT    /posts/:id
-//    DELETE /posts/:id
-
-// There are other styles for creating these route handlers, we typically
-// explore other patterns to reduce code duplication.
-// TODO: Can you spot where we have some duplication below?
+//    GET    /micro_posts
+//    POST   /micro_posts
+//    GET    /micro_posts/:id
+//    PUT    /micro_posts/:id
+//    DELETE /micro_posts/:id
 
 router.get("/", (req, res) => {
-  Post.findAll({}).then((posts) => res.json(posts));
+  MicroPost.findAll({}).then((allPosts) => res.json(allPosts));
 });
 
 router.post("/", (req, res) => {
   let { content } = req.body;
 
-  Post.create({ content })
-    .then((post) => {
-      res.status(201).json(post);
+  MicroPost.create({ content })
+    .then((newPost) => {
+      res.status(201).json(newPost);
     })
     .catch((err) => {
       res.status(400).json(err);
@@ -33,27 +29,27 @@ router.post("/", (req, res) => {
 
 router.get("/:id", (req, res) => {
   const { id } = req.params;
-  Post.findByPk(id).then((post) => {
-    if (!post) {
+  MicroPost.findByPk(id).then((mpost) => {
+    if (!mpost) {
       return res.sendStatus(404);
     }
 
-    res.json(post);
+    res.json(mpost);
   });
 });
 
 router.put("/:id", (req, res) => {
   const { id } = req.params;
-  Post.findByPk(id).then((post) => {
-    if (!post) {
+  MicroPost.findByPk(id).then((mpost) => {
+    if (!mpost) {
       return res.sendStatus(404);
     }
 
-    post.content = req.body.content;
-    post
+    mpost.content = req.body.content;
+    mpost
       .save()
-      .then((post) => {
-        res.json(post);
+      .then((updatedPost) => {
+        res.json(updatedPost);
       })
       .catch((err) => {
         res.status(400).json(err);
@@ -63,12 +59,12 @@ router.put("/:id", (req, res) => {
 
 router.delete("/:id", (req, res) => {
   const { id } = req.params;
-  Post.findByPk(id).then((post) => {
-    if (!post) {
+  MicroPost.findByPk(id).then((mpost) => {
+    if (!mpost) {
       return res.sendStatus(404);
     }
 
-    post.destroy();
+    mpost.destroy();
     res.sendStatus(204);
   });
 });
