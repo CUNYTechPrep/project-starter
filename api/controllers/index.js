9690b191eb81c997f6887b8e0072d85591ccabd8
Edgardo Molina <medgardo@gmail.com>
2022-11-02 16:45:45 -0400

    (auth) backend: add auth controller

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 // Load each controller
 const microPostsController = require("./microPosts.js");
+const authController = require("./auth.js");
 
 // Mount each controller under a specific route. These
 // will be prefixes to all routes defined inside the controller
+router.use("/auth", authController);
 router.use("/micro_posts", microPostsController);
 
 module.exports = router;
