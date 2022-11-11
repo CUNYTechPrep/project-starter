4b721dcb4fa1c8bbb55a5622391a031c013935d7
Jaichyi Chen <jaichyichen@gmail.com>
2022-11-11 18:30:42 -0500

    added Map components, downloaded axios

--- client/src/components/HomeCard.js
+++ client/src/components/HomeCard.js
@@ -1,32 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Map from "./Map";
 
-
-
-function HomeCard({  address, electric, gas, mortgage, rent, step, tenanted, water, id }) {
-const tempImgUrl = "https://streetviewpixels-pa.googleapis.com/v1/thumbnail?output=thumbnail&cb_client=maps_sv.tactile.gps&panoid=rpTI7Jo7WnpqqMkArgq5WQ&w=883&h=435&thumb=2&yaw=71.51634&pitch=0";
+function HomeCard({
+  address,
+  electric,
+  gas,
+  mortgage,
+  rent,
+  step,
+  tenanted,
+  water,
+  id,
+}) {
+  const tempImgUrl =
+    "https://streetviewpixels-pa.googleapis.com/v1/thumbnail?output=thumbnail&cb_client=maps_sv.tactile.gps&panoid=rpTI7Jo7WnpqqMkArgq5WQ&w=883&h=435&thumb=2&yaw=71.51634&pitch=0";
 
   return (
     <div className="col-10 col-md-8 col-lg-7">
       <div className="card mb-4 shadow">
         <div className="card-body card-text">
-            <img src={tempImgUrl} 
-                class="img-fluid"
-                alt="House"/>
+          <Map address={address}></Map>
         </div>
 
-        <ul class="list-group list-group-flush">
-                <li class="list-group-item">          
-                    Address: <Link to={"/posts/" + id}>{address}</Link>
-                </li>
-            
-                <div class="container">
-                    <div class="row">
-                            <li class="list-group-item col-sm">rent: {rent}</li>
-                            <li class="list-group-item col-sm">mortgage: {mortgage}</li>
-                    </div>
-                </div>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            Address: <Link to={"/posts/" + id}>{address}</Link>
+          </li>
 
+          <div className="container">
+            <div className="row">
+              <li className="list-group-item col-sm">rent: {rent}</li>
+              <li className="list-group-item col-sm">mortgage: {mortgage}</li>
+            </div>
+          </div>
         </ul>
 
         <div className="card-footer small text-muted text-end">Other Info</div>
---
+++ client/src/components/Map.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import {
+  GoogleMap,
+  LoadScript,
+  StreetViewPanorama,
+} from "@react-google-maps/api";
+
+const Map = (props) => {
+  const [center, setCenter] = useState({ lat: 0, lng: 0 });
+  const { address } = props;
+  useEffect(() => {
+    geoCode();
+  }, [address]);
+
+  const geoCode = () => {
+    axios
+      .get("https://maps.googleapis.com/maps/api/geocode/json", {
+        params: {
+          address: address,
+          key: process.env.REACT_APP_API_KEY,
+        },
+      })
+      .then(function (res) {
+        // const converted = res.data.results[0].geometry.location;
+        setCenter({
+          lat: res.data.results[0].geometry.location.lat,
+          lng: res.data.results[0].geometry.location.lng,
+        });
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  };
+  //   const url = `https://maps.googleapis.com/maps/api/streetview?size=400x400&location=${lat},${lng}
+  // &key=${process.env.REACT_APP_API_KEY}`;
+  return (
+    <LoadScript googleMapsApiKey={process.env.REACT_APP_API_KEY}>
+      <GoogleMap
+        mapContainerStyle={{ width: "100%", height: "400px" }}
+        center={center}
+        zoom={7}
+      >
+        <StreetViewPanorama
+          position={center}
+          visible={true}
+          options={{ disableDefaultUI: true, scrollwheel: false }}
+        />
+      </GoogleMap>
+    </LoadScript>
+  );
+};
+
+export default Map;
