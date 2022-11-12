47c9c0cefec63ae8876e5944f4c1d47817579a44
tshetrim <tshetrim123@gmail.com>
2022-11-11 19:23:48 -0500

    Define backend api routes, logic not yet implemented

--- api/controllers/houses.js
+++ api/controllers/houses.js
@@ -2,109 +2,123 @@ const express = require("express");
 const { TableHints } = require("sequelize");
 const router = express.Router();
 const db = require("../models");
-const { MicroPost, Property } = db;
+const { UserInfo, Bill, House, Rent } = db;
 
-
+//return json of houses based on userID
 router.get("/", (req, res) => {
-  Property.findAll({}).then((allHouses) => res.json(allHouses));
+	Property.findAll({}).then((allHouses) => res.json(allHouses));
 });
 
-router.post("/", (req, res) => {
-  let { content } = req.body;
-
-  MicroPost.create({ content })
-    .then((newPost) => {
-      res.status(201).json(newPost);
-    })
-    .catch((err) => {
-      res.status(400).json(err);
-    });
-});
-
-router.post("/form", (req, res) => {
-  let { address, electric, gas, mortgage, rent, step, tenanted, water } = req.body;
-
-  Property.create({ address, electric, gas, mortgage, rent, step, tenanted, water })
-    .then((newPost) => {
-      res.status(201).json(newPost);
-    })
-    .catch((err) => {
-      res.status(400).json(err);
-    });
-});
-
+//return house specific information
 router.get("/house/:id", (req, res) => {
-  const { id } = req.params;
-  Property.findByPk(id).then((mpost) => {
-    if (!mpost) {
-      return res.sendStatus(404);
-    }
-    res.json(mpost);
-  });
+	const { id } = req.params;
+	House.findByPk(id).then((house) => {
+		if (!house) {
+			return res.sendStatus(404);
+		}
+		res.json(house);
+	});
+});
+
+// delete a house based on id
+router.delete("/house/:id", (req, res) => {
+	const { id } = req.params;
+	House.findByPk(id).then((house) => {
+		if (!house) {
+			return res.sendStatus(404);
+		}
+		house.destroy();
+		res.sendStatus(204);
+	});
+});
+
+//update billsTable with all bill information
+router.post("/form", (req, res) => {
+	let { address, electric, gas, mortgage, rent, step, tenanted, water } =
+		req.body;
+
+	Property.create({
+		address,
+		electric,
+		gas,
+		mortgage,
+		rent,
+		step,
+		tenanted,
+		water,
+	})
+		.then((newPost) => {
+			res.status(201).json(newPost);
+		})
+		.catch((err) => {
+			res.status(400).json(err);
+		});
+});
+
+//get a bill from billTable
+router.get("/house/:id/bills/billType/:billType?", (req, res) => {
+	const { id } = req.params;
+	Bill.findByPk(id).then((mpost) => {
+		if (!mpost) {
+			return res.sendStatus(404);
+		}
+		res.json(mpost);
+	});
+});
+
+//add a bill to billTable
+router.post("/house/:id/bills/billType/:billType?", (req, res) => {
+	const { id } = req.params;
+});
+
+//modify a bill in the billTable
+router.put("/house/:id/bills/billType/:billType?", (req, res) => {
+	const { id } = req.params;
+});
+
+//delete a bill in billTable
+router.delete("/house/:id/bills/billType/:billType?", (req, res) => {
+	const { id } = req.params;
 });
 
 
-// router.get("/house/:id/tableName/:table?", (req, res) => {
+//get a rent from rent table
+router.get("/house/:id/rents", (req, res) => {
+	const { id } = req.params;
+});
+
+//add a rent to rentTable
+router.post("/house/:id/rents", (req, res) => {
+	const { id } = req.params;
+});
+
+//modify a rent in the rentTable
+router.put("/house/:id/rents", (req, res) => {
+	const { id } = req.params;
+});
+
+//delete a rent  in rentTable
+router.delete("/house/:id/rents", (req, res) => {
+	const { id } = req.params;
+});
+
+// router.put("/:id", (req, res) => {
 //   const { id } = req.params;
-//   Property.findByPk(id).then((mpost) => {
+//   MicroPost.findByPk(id).then((mpost) => {
 //     if (!mpost) {
 //       return res.sendStatus(404);
 //     }
-//     res.json(mpost);
+
+//     mpost.content = req.body.content;
+//     mpost
+//       .save()
+//       .then((updatedPost) => {
+//         res.json(updatedPost);
+//       })
+//       .catch((err) => {
+//         res.status(400).json(err);
+//       });
 //   });
 // });
-
-// router.post("/house/:id/tableName/:table?", (req, res) => {
-//   const { id } = req.params;
-//   Property.findByPk(id).then((mpost) => {
-//     if (!mpost) {
-//       return res.sendStatus(404);
-//     }
-//     res.json(mpost);
-//   });
-// });
-
-
-router.get("/:id", (req, res) => {
-  const { id } = req.params;
-  MicroPost.findByPk(id).then((mpost) => {
-    if (!mpost) {
-      return res.sendStatus(404);
-    }
-
-    res.json(mpost);
-  });
-});
-
-router.put("/:id", (req, res) => {
-  const { id } = req.params;
-  MicroPost.findByPk(id).then((mpost) => {
-    if (!mpost) {
-      return res.sendStatus(404);
-    }
-
-    mpost.content = req.body.content;
-    mpost
-      .save()
-      .then((updatedPost) => {
-        res.json(updatedPost);
-      })
-      .catch((err) => {
-        res.status(400).json(err);
-      });
-  });
-});
-
-router.delete("/:id", (req, res) => {
-  const { id } = req.params;
-  MicroPost.findByPk(id).then((mpost) => {
-    if (!mpost) {
-      return res.sendStatus(404);
-    }
-
-    mpost.destroy();
-    res.sendStatus(204);
-  });
-});
 
 module.exports = router;
