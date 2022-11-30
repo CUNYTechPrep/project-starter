f7cc41a997b93075f97057a9ebec56e973376e2c
edis123 <edismur2@gmail.com>
2022-11-30 13:16:13 -0500

    [controllersPart1]

--- api/models/Media.js
+++ api/models/Media.js
@@ -1,6 +1,4 @@
 "use strict";
-import fetch from 'node-fetch';
-
 
 const { Model } = require("sequelize");
 
@@ -9,47 +7,33 @@ const { Model } = require("sequelize");
 
 module.exports = (sequelize, DataTypes) => {
   class Media extends Model {}
-// The Media table : [MediaId | AVGRating]
 
-
-    function myUrl(titleInput) {// setting the url
-
-        let url = 'https://api.tvmaze.com/shows?q=' + titleInput
-        return url
-    }
-    async function getIMBD() { // fetching // not sure if this works
-        const response = await fetch(myUrl(titleInput));
-        const data = await response.json();
-        const imbd = data.imbd;
-        return imbd;
-    }
 
 
   Media.init(
     {
       MediaID :{
         type:DataTypes.STRING,
+        primaryKey:true,
         validate:{
             notEmpty:true
+        }
         },
-        AVGRating: {
-            type:DataTypes.INTEGER,  // do not store , calcuate on query
-           //need to think about it 
-           // feel free to implement
-           defaultValue: 0
+        AvgRating: {
+            type:DataTypes.REAL,  // do not store , calcuate on query
+           defaultValue: 0,
+           validate:{
+            notEmpty:true
+           }
         }
         
-      }
-    },
+      },
+    
     {
       sequelize,
       modelName: "Media",
     }
   );
-    //  const media = new Media({ id: getIMBD() }); //assigning the ID to the mediaID
-    //      media.id;
-
-// 
 
   return Media;
 };
