46e00bdcdbff01436f6f1876445133c91760f8bf
Anthony Duran <87147191+Code1Here@users.noreply.github.com>
2022-11-30 10:28:35 -0500

    Transaction name, price and date

--- client/src/components/MicroPostCard.js
+++ client/src/components/MicroPostCard.js
@@ -2,13 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function MicroPostCard({ content, createdAt, id }) {
+  const total = 10;
   return (
     <div className="col-10 col-md-8 col-lg-7">
-      <div className="card mb-4 shadow">
-        <div className="card-body card-text">
+      <div className="d-flex card">
+        <div className="me-auto p-2 card-body card-text">
           <Link to={"/posts/" + id}>{content}</Link>
         </div>
-        <div className="card-footer small text-muted text-end">{createdAt}</div>
+        <div>{total}</div>
+        <div className="card-footer small text-muted text-end">
+          {createdAt.slice(0, -14)}
+        </div>
       </div>
     </div>
   );
