8376abfb5163951aa9933d5b3e15936cf4cacbdc
Affat25 <affat9422@gmail.com>
2022-12-04 16:16:10 -0500

    add transaction working, waiting on backend

--- client/src/components/AddTransaction.js
+++ client/src/components/AddTransaction.js
@@ -1,11 +1,50 @@
-import React from "react";
+import React, { useState }  from "react";
 import {Modal, Button, ModalBody, ModalTitle, ModalHeader, Form, ModalFooter, FormCheck, FormGroup, FormControl, Row, Col} from "react-bootstrap";
+import ErrorAlert from "../components/ErrorAlert";
 
 function AddTransaction() {
-    const [isShow,invokeModal]= React.useState(false)
+    const [content, setContent] = useState("");
+    const [error, setError] = useState(false);
+    const [isShow,invokeModal]= useState(false);
+    
+    const handleContentChange = (event) => {
+      setContent(event.target.value);
+    };
+
+    const handleSubmit = async (event) => {
+      try {
+        let response = await fetch("/api/micro_posts", {
+          method: "POST",
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            content: content,
+          }),
+        });
+  
+        if (response.ok) {
+          invokeModal(false);
+          window.location.reload(false);
+
+        } else {
+          setError(true);
+        }
+      } catch (error) {
+        console.error("Server error while creating a new micro post", error);
+        setError(true);
+      }
+
+    };
+  
+  
     const initModal=()=>{
       return invokeModal(!isShow)
     }
+
+    if(error) return <ErrorAlert details={"Failed to save the content"} />;
+
     return(
       <div>
       <Button variant="primary" onClick={initModal}>
@@ -18,7 +57,7 @@ function AddTransaction() {
         <ModalBody>
           <Form>
             <FormGroup className="mb-3" controlId="formTransaction">
-              <FormControl type="text" placeholder="Name"/>
+              <FormControl type="text" placeholder="Name" value={content} onChange={handleContentChange}/>
             </FormGroup>
             <FormGroup className="mb-3" controlId="formAmount">
               <FormControl type="number" placeholder="Amount"/>
@@ -45,7 +84,7 @@ function AddTransaction() {
           <Button variant="secondary" onClick={initModal}>
             Cancel
           </Button>
-          <Button type="submit" variant="success" onClick={initModal}>
+          <Button type="submit" variant="success" onClick={handleSubmit}>
             Confirm
           </Button>
         </ModalFooter>
