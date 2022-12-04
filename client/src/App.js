7a3fef0a348df632f74ab1d62aa80b7112b045fc
Anthony Duran <anthonybduran@gmail.com>
2022-12-03 22:38:08 -0500

    Models, About Usc, Sign up, Posts

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import PostsListPage from "./pages/PostsListPage";
 import PostFormPage from "./pages/PostFormPage";
 import ShowPostPage from "./pages/ShowPostPage";
 import AboutUsPage from "./pages/AboutUsPage";
+import SignUpPage from "./pages/SignUpPage";
 
 import "./App.css";
 
@@ -25,6 +26,11 @@ function Navigation(props) {
               About Us
             </NavLink>
           </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/form">
+              Fill-out Form
+            </NavLink>
+          </li>
         </ul>
       </div>
     </nav>
@@ -35,13 +41,18 @@ function App() {
   return (
     <BrowserRouter>
       <Navigation />
-      <div className="container-xl text-center">
-        <div className="row justify-content-center">
+      <div className="container-xl ">
+        {" "}
+        {/*text-center*/}
+        <div className="row ">
+          {" "}
+          {/*justify-content-center*/}
           <Routes>
             <Route path="/posts/new" element={<PostFormPage />} />
             <Route path="/posts/:id" element={<ShowPostPage />} />
             <Route path="/about-us" element={<AboutUsPage />} />
             <Route path="/" element={<PostsListPage />} />
+            <Route path="/form" element={<SignUpPage />} />
           </Routes>
         </div>
       </div>
---
+++ client/src/pages/SignUpPage.js
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+
+function SignUpPage(props) {
+  const [userInfo, setUserInfo] = useState([]);
+
+  return (
+    <>
+      <div className="container mt-5">
+        <h1>Sign up</h1>
+        <form className="row g-3">
+          <div className="col-md-6">
+            <label for="firstName" className="form-label">
+              First Name *
+            </label>
+            <input type="text" className="form-control" id="firstName" required></input>
+          </div>
+          <div className="col-md-6">
+            <label for="lastName" className="form-label">
+              Last Name *
+            </label>
+            <input type="text" className="form-control" id="lastName" required></input>
+          </div>
+          <div className="col-md-10">
+            <label for="emailInfo" className="form-label">
+              Email *
+            </label>
+            <input type="email" className="form-control" id="emailInfo" required></input>
+          </div>
+          <div className="col-md-2">
+            <label for="birthDate" className="form-label">
+              Date of birth *
+            </label>
+            <input type="date" className="form-control" id="birthDate" required></input>
+          </div>
+
+          {/* vvvvvvvvv */}
+
+          <div className="col-md-6 dropdown">
+            <label for="genderIdentity" className="form-label">
+              Gender Identity
+            </label>
+
+            <button className="btn dropdown-toggle border text-end form-control text-muted" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Please Select{" "}
+            </button>
+            <ul className="dropdown-menu">
+              <li>
+                <a className="dropdown-item" href="/">
+                  Action
+                </a>
+              </li>
+              <li>
+                <a className="dropdown-item" href="/">
+                  Another
+                </a>{" "}
+                action
+              </li>
+              <li>
+                <a className="dropdown-item" href="/">
+                  Something
+                </a>{" "}
+                else here
+              </li>
+            </ul>
+          </div>
+
+          {/* ^^^^^^^^^ */}
+
+          <div className="col-md-6">
+            <label for="monthlyIncome" className="form-label">
+              Monthly Income *
+            </label>
+            <input type="text" className="form-control" id="monthlyIncome" required></input>
+          </div>
+          <div className="col-md-12">
+            <label for="comments" className="form-label">
+              About me
+            </label>
+            <textarea className="form-control" id="comments" rows="3" placeholder="Say something good about yourself! You could mention a fund target goal!"></textarea>
+          </div>
+          <div className="col-md-12">
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default SignUpPage;
