a6c3115a0c1bf22016072c5207fc25d588f5264b
Edgardo Molina <edgardo@cunytechprep.org>
2024-08-31 02:35:40 -0400

    Add 2023 project starter react components

--- frontend-client/src/App.jsx
+++ frontend-client/src/App.jsx
@@ -1,34 +1,51 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React from "react";
+import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
+import PostsListPage from "./pages/PostsListPage";
+import PostFormPage from "./pages/PostFormPage";
+import ShowPostPage from "./pages/ShowPostPage";
+import AboutUsPage from "./pages/AboutUsPage";
+
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+function Navigation(props) {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <nav className="navbar navbar-expand-sm navbar-dark bg-dark shadow mb-3">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">
+          Micro Blog
+        </Link>
+        <ul className="navbar-nav me-auto">
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/posts/new">
+              Create a Micro Post
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/about-us">
+              About Us
+            </NavLink>
+          </li>
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+    </nav>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navigation />
+      <div className="container-xl text-center">
+        <div className="row justify-content-center">
+          <Routes>
+            <Route path="/posts/new" element={<PostFormPage />} />
+            <Route path="/posts/:id" element={<ShowPostPage />} />
+            <Route path="/about-us" element={<AboutUsPage />} />
+            <Route path="/" element={<PostsListPage />} />
+          </Routes>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </BrowserRouter>
   );
 }
 
