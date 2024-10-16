4ced80cf8ba441aca6cd437cea6f10e4abd41966
Edgardo Molina <edgardo@cunytechprep.org>
2024-10-16 10:59:33 -0400

    Remove all files

--- client/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-
-const htmlContainer = document.getElementById("root");
-const root = ReactDOM.createRoot(htmlContainer);
-
-root.render(<App />);
