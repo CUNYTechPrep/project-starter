de8d2d295b82d8639cdcde9534bec077869c06e1
ravimultani38 <ravi.multani38@gmail.com>
2024-11-06 20:50:11 -0500

    add delete button

--- frontend-client/src/components/MicroPostCard.jsx
+++ frontend-client/src/components/MicroPostCard.jsx
@@ -1,13 +1,21 @@
 import { Link } from "react-router-dom";
 
-function MicroPostCard({ content, createdAt, id }) {
+function MicroPostCard({ content, createdAt, id, onDelete }) {
   return (
     <div className="col-10 col-md-8 col-lg-7">
       <div className="card mb-4 shadow">
         <div className="card-body card-text">
           <Link to={"/posts/" + id}>{content}</Link>
         </div>
-        <div className="card-footer small text-muted text-end">{createdAt}</div>
+        <div className="card-footer d-flex justify-content-between align-items-center small text-muted">
+          <span>{createdAt}</span>
+          <button
+            onClick={() => onDelete(id)}
+            className="btn btn-sm btn-danger"
+          >
+            Delete
+          </button>
+        </div>
       </div>
     </div>
   );
--- frontend-client/src/pages/PostsListPage.jsx
+++ frontend-client/src/pages/PostsListPage.jsx
@@ -29,6 +29,21 @@ function PostsListPage() {
     };
   }, []);
 
+  const handleDelete = async (postId) => {
+    try {
+      const response = await fetch(`/api/micro_posts/${postId}`, {
+        method: "DELETE",
+      });
+      if (response.ok) {
+        setPosts(posts.filter((post) => post.id !== postId));
+      } else {
+        console.error("Failed to delete the post");
+      }
+    } catch (error) {
+      console.error("Error deleting the post", error);
+    }
+  };
+
   if (error) return <ErrorAlert details="Failed to fetch all micro posts" />;
   if (loading) return <LoadingSpinner />;
 
@@ -36,7 +51,11 @@ function PostsListPage() {
     <div className="container-fluid text-center">
       <div className="row justify-content-center">
         {posts.map((entryData) => (
-          <MicroPostCard {...entryData} key={entryData.id} />
+          <MicroPostCard
+            {...entryData}
+            key={entryData.id}
+            onDelete={() => handleDelete(entryData.id)}
+          />
         ))}
       </div>
     </div>
